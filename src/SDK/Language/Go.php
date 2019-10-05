3e80f001103e5711365777d1b29a2fc11a6b2f6f
Mattia Panzeri <mattia.panzeri93@gmail.com>
2019-10-06 00:02:07 +0200

    Work in progress - Go templates setup

---
+++ src/SDK/Language/Go.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace Appwrite\SDK\Language;
+
+use Appwrite\SDK\Language;
+
+class Go extends Language {
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Go';
+    }
+
+    /**
+     * Get Language Keywords List
+     *
+     * @return array
+     */
+    public function getKeywords()
+    {
+        return [
+            'bool',
+            'error',
+            'for',
+            'func',
+            'go',
+            'if',
+            'import',
+            'make',
+            'map',
+            'nil',
+            'package',
+            'range',
+			'return',
+            'string',
+            'struct',
+            'type',
+            'var',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getFiles()
+    {
+        return [
+            [
+                'scope'         => 'default',
+                'destination'   => 'main.go',
+                'template'      => '/go/main.go.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => 'README.md',
+                'template'      => '/go/README.md.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => 'LICENSE',
+                'template'      => '/go/LICENSE.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => 'client.go',
+                'template'      => '/go/client.go.twig',
+                'minify'        => false,
+            ],
+        ];
+    }
+
+    /**
+     * @param $type
+     * @return string
+     */
+    public function getTypeName($type)
+    {
+        switch ($type) {
+            case self::TYPE_INTEGER:
+            case self::TYPE_NUMBER:
+                return 'number';
+            break;
+            case self::TYPE_FILE:
+                return 'File';
+            break;
+        }
+
+        return $type;
+    }
+
+    /**
+     * @param array $param
+     * @return string
+     */
+    public function getParamDefault(array $param)
+    {
+        $type       = (isset($param['type'])) ? $param['type'] : '';
+        $default    = (isset($param['default'])) ? $param['default'] : '';
+        $required   = (isset($param['required'])) ? $param['required'] : '';
+
+        if($required) {
+            return '';
+        }
+
+        $output = ' = ';
+
+        if(empty($default) && $default !== 0 && $default !== false) {
+            switch ($type) {
+                case self::TYPE_NUMBER:
+                case self::TYPE_INTEGER:
+                case self::TYPE_BOOLEAN:
+                    $output .= 'null';
+                    break;
+                case self::TYPE_STRING:
+                    $output .= "''";
+                    break;
+                case self::TYPE_ARRAY:
+                    $output .= '[]';
+                    break;
+            }
+        }
+        else {
+            switch ($type) {
+                case self::TYPE_NUMBER:
+                case self::TYPE_INTEGER:
+                case self::TYPE_ARRAY:
+                    $output .= $default;
+                    break;
+                case self::TYPE_BOOLEAN:
+                    $output .= ($default) ? 'true' : 'false';
+                    break;
+                case self::TYPE_STRING:
+                    $output .= "'{$default}'";
+                    break;
+            }
+        }
+
+        return $output;
+    }
+
+    /**
+     * @param array $param
+     * @return string
+     */
+    public function getParamExample(array $param)
+    {
+        $type       = (isset($param['type'])) ? $param['type'] : '';
+        $example    = (isset($param['example'])) ? $param['example'] : '';
+
+        $output = '';
+
+        if(empty($example) && $example !== 0 && $example !== false) {
+            switch ($type) {
+                case self::TYPE_NUMBER:
+                case self::TYPE_INTEGER:
+                case self::TYPE_BOOLEAN:
+                    $output .= 'null';
+                    break;
+                case self::TYPE_STRING:
+                    $output .= "''";
+                    break;
+                case self::TYPE_ARRAY:
+                    $output .= '[]';
+                    break;
+                case self::TYPE_FILE:
+                    $output .= "document.getElementById('uploader').files[0]";
+                    break;
+            }
+        }
+        else {
+            switch ($type) {
+                case self::TYPE_NUMBER:
+                case self::TYPE_INTEGER:
+                case self::TYPE_ARRAY:
+                    $output .= $example;
+                    break;
+                case self::TYPE_BOOLEAN:
+                    $output .= ($example) ? 'true' : 'false';
+                    break;
+                case self::TYPE_STRING:
+                    $output .= "'{$example}'";
+                    break;
+                case self::TYPE_FILE:
+                    $output .= "document.getElementById('uploader').files[0]";
+                    break;
+            }
+        }
+
+        return $output;
+    }
+}
