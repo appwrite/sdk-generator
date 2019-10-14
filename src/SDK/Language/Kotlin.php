83c5c1b989bc52be2cc8e6a074b9ace8eb572abf
unknown <tomerpacific@gmail.com>
2019-10-14 11:38:14 +0300

    Created Kotlin.php and added skeleton of API methods

---
+++ src/SDK/Language/Kotlin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Appwrite\SDK\Language;
+
+use Appwrite\SDK\Language;
+
+class Kotlin extends Language {
+
+	/**
+	 * @return @string
+	 **/
+	public function getName() 
+	{
+		return 'Kotlin'
+	}
+
+	/**
+     * @return array
+     */
+	public function getKeywords() 
+	{
+		return [
+
+		]
+	}
+
+	/**
+     * @return array
+     */
+	public function getFiles(): Array 
+	{
+		return [
+
+		]
+	}
+
+	/**
+     * @param $type
+     * @return string
+     */
+	public function getTypeName($type) 
+	{
+
+	}
+
+	/**
+     * @param array $param
+     * @return string
+     */
+	public function getParamDefault(array $param)
+	{
+
+	}
+
+	/**
+     * @param array $param
+     * @return string
+     */
+	public function getParamExample(array $param) 
+	{
+
+	}
+}
