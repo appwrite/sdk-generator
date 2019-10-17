ba853841839edac8c294f36f73e2382b2d8bcfc7
eldadfux <eldad.fux@gmail.com>
2019-10-17 07:27:19 +0300

    Added tests for multiple PHP versions

--- tests/SDKTest.php
+++ tests/SDKTest.php
@@ -10,7 +10,12 @@ class SDKTest extends TestCase
      * @var Client
      */
     protected $containers = [
-        'php' => 'docker run --rm -v $(pwd):/app -w /app php:7.3-cli php tests/languages/tests-for-php.php',
+        'php-5.6' => 'docker run --rm -v $(pwd):/app -w /app php:5.6-cli php tests/languages/tests-for-php.php',
+        'php-7.0' => 'docker run --rm -v $(pwd):/app -w /app php:7.0-cli php tests/languages/tests-for-php.php',
+        'php-7.1' => 'docker run --rm -v $(pwd):/app -w /app php:7.1-cli php tests/languages/tests-for-php.php',
+        'php-7.2' => 'docker run --rm -v $(pwd):/app -w /app php:7.2-cli php tests/languages/tests-for-php.php',
+        'php-7.3' => 'docker run --rm -v $(pwd):/app -w /app php:7.3-cli php tests/languages/tests-for-php.php',
+        //'nodejs-12.12' => 'docker run --rm -v $(pwd):/app -w /app node:12.12 node tests/languages/tests-for-node.js',
     ];
 
     public function setUp()
@@ -36,6 +41,8 @@ class SDKTest extends TestCase
             $output = [];
 
             exec($command, $output);
+
+            var_dump($key, $command, $output);
 
             $this->assertEquals($output[0], 'GET:/v1/mock/tests/foo:passed');
             $this->assertEquals($output[1], 'POST:/v1/mock/tests/foo:passed');
