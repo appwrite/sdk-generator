883443e8199940701c7672b6111035576b6d0fde
eldadfux <eldad.fux@gmail.com>
2019-10-18 09:04:48 +0300

    Updated test script structure

--- tests/SDKTest.php
+++ tests/SDKTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests;
 
+use Appwrite\Spec\Swagger2;
+use Appwrite\SDK\SDK;
 use PHPUnit\Framework\TestCase;
 
 class SDKTest extends TestCase
@@ -9,20 +11,26 @@ class SDKTest extends TestCase
     /**
      * @var array
      */
-    protected $containers = [
-        'php-5.6' => 'docker run --rm -v $(pwd):/app -w /app php:5.6-cli php tests/php/test.php',
-        'php-7.0' => 'docker run --rm -v $(pwd):/app -w /app php:7.0-cli php tests/php/test.php',
-        'php-7.1' => 'docker run --rm -v $(pwd):/app -w /app php:7.1-cli php tests/php/test.php',
-        'php-7.2' => 'docker run --rm -v $(pwd):/app -w /app php:7.2-cli php tests/php/test.php',
-        'php-7.3' => 'docker run --rm -v $(pwd):/app -w /app php:7.3-cli php tests/php/test.php',
-        //'nodejs-12.12' => 'docker run --rm -v $(pwd):/app -w /app node:12.12 node tests/languages/tests-for-node.js',
-    ];
-
-    /**
-     * @var array
-     */
-    protected $packaging = [
-        'node' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/node node:12.12 npm install',
+    protected $languages  = [
+        'php' => [
+            'class' => 'Appwrite\SDK\Language\PHP',
+            'build' => 'docker run --rm -v $(pwd):/app -w /app php:7.3-cli php -v',
+            'envs' => [
+                'php-5.6' => 'docker run --rm -v $(pwd):/app -w /app php:5.6-cli php tests/php/test.php',
+                'php-7.0' => 'docker run --rm -v $(pwd):/app -w /app php:7.0-cli php tests/php/test.php',
+                'php-7.1' => 'docker run --rm -v $(pwd):/app -w /app php:7.1-cli php tests/php/test.php',
+                'php-7.2' => 'docker run --rm -v $(pwd):/app -w /app php:7.2-cli php tests/php/test.php',
+                'php-7.3' => 'docker run --rm -v $(pwd):/app -w /app php:7.3-cli php tests/php/test.php',
+            ],
+            
+        ],
+        'node' => [
+            'class' => 'Appwrite\SDK\Language\Node',
+            'build' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/node node:12.12 npm install',
+            'envs' => [
+                //'nodejs-12.12' => 'docker run --rm -v $(pwd):/app -w /app node:12.12 node tests/languages/tests-for-node.js',
+            ],
+        ],
     ];
 
     public function setUp()
@@ -42,42 +50,56 @@ class SDKTest extends TestCase
          */
 
         echo "\n";
-        
+
         echo "Generating SDKs files for all langauges...\n";
 
-        exec('docker run --rm -v $(pwd):/app -w /app php:7.3-cli php tests/resources/generate.php', $output);
+        try {
+            $spec = file_get_contents(__DIR__ . '/resources/spec.json');
+        
+            if(empty($spec)) {
+                throw new Exception('Failed to fetch spec from Appwrite server');
+            }
+        
+            foreach ($this->languages as $language => $options) {
+                $sdk  = new SDK(new $options['class'](), new Swagger2($spec));
+        
+                $sdk
+                    ->setLicenseContent('demo license')
+                ;
+        
+                $sdk->generate(__DIR__ . '/sdks/' . $language);
+                
+                $output = [];
 
-        $this->assertEquals($output[0], 'Example SDKs generated successfully');
+                echo "Building {$language} package...\n";
+    
+                exec($options['build'], $output);
 
-        foreach ($this->packaging as $key => $command) {
-            $output = [];
-
-            echo "Installing {$key} package dependencies...\n";
-
-            exec($command, $output);
-
-            //var_dump($output);
+                foreach ($options['envs'] as $key => $command) {
+                    echo "Running tests for the {$key} environment...\n";
+                    
+                    $output = [];
+        
+                    exec($command, $output);
+        
+                    $this->assertEquals($output[0], 'GET:/v1/mock/tests/foo:passed');
+                    $this->assertEquals($output[1], 'POST:/v1/mock/tests/foo:passed');
+                    $this->assertEquals($output[2], 'PUT:/v1/mock/tests/foo:passed');
+                    $this->assertEquals($output[3], 'PATCH:/v1/mock/tests/foo:passed');
+                    $this->assertEquals($output[4], 'DELETE:/v1/mock/tests/foo:passed');
+                    $this->assertEquals($output[5], 'GET:/v1/mock/tests/bar:passed');
+                    $this->assertEquals($output[6], 'POST:/v1/mock/tests/bar:passed');
+                    $this->assertEquals($output[7], 'PUT:/v1/mock/tests/bar:passed');
+                    $this->assertEquals($output[8], 'PATCH:/v1/mock/tests/bar:passed');
+                    $this->assertEquals($output[9], 'DELETE:/v1/mock/tests/bar:passed');
+                }
+            }
         }
-
-        foreach ($this->containers as $key => $command) {
-            echo "Running tests for the {$key} environment...\n";
-            
-            $output = [];
-
-            exec($command, $output);
-
-            //var_dump($key, $command, $output);
-
-            $this->assertEquals($output[0], 'GET:/v1/mock/tests/foo:passed');
-            $this->assertEquals($output[1], 'POST:/v1/mock/tests/foo:passed');
-            $this->assertEquals($output[2], 'PUT:/v1/mock/tests/foo:passed');
-            $this->assertEquals($output[3], 'PATCH:/v1/mock/tests/foo:passed');
-            $this->assertEquals($output[4], 'DELETE:/v1/mock/tests/foo:passed');
-            $this->assertEquals($output[5], 'GET:/v1/mock/tests/bar:passed');
-            $this->assertEquals($output[6], 'POST:/v1/mock/tests/bar:passed');
-            $this->assertEquals($output[7], 'PUT:/v1/mock/tests/bar:passed');
-            $this->assertEquals($output[8], 'PATCH:/v1/mock/tests/bar:passed');
-            $this->assertEquals($output[9], 'DELETE:/v1/mock/tests/bar:passed');
+        catch (Exception $exception) {
+            echo 'Error: ' . $exception->getMessage() . ' on ' . $exception->getFile() . ':' . $exception->getLine() . "\n";
+        }
+        catch (Throwable $exception) {
+            echo 'Error: ' . $exception->getMessage() . ' on ' . $exception->getFile() . ':' . $exception->getLine() . "\n";
         }
 
         exec('rm -r ./tests/sdks');
