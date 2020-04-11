69f332fc118a948694d6034f8af2b72fd5e86284
Eldad Fux <eldad.fux@gmail.com>
2020-04-11 19:29:01 +0300

    Fix for file upload

--- src/SDK/Language/Dart.php
+++ src/SDK/Language/Dart.php
@@ -99,7 +99,7 @@ class Dart extends Language {
                 return 'String';
             break;
             case self::TYPE_FILE:
-                return 'File';
+                return 'MultipartFile';
             break;
             case self::TYPE_BOOLEAN:
                 return 'bool';
@@ -186,7 +186,7 @@ class Dart extends Language {
         if(empty($example) && $example !== 0 && $example !== false) {
             switch ($type) {
                 case self::TYPE_FILE:
-                    $output .= 'new File(\'./image.jpg\')';
+                    $output .= 'MultipartFile.fromFile(\'./path-to-files/image.jpg\', \'image.jpg\')';
                     break;
                 case self::TYPE_NUMBER:
                 case self::TYPE_INTEGER:
