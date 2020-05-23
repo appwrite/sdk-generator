9b04a1a8ee0c6d3d3331f50160bd41d6646e7939
Ali Almoullim <ali.almoullim@gmail.com>
2020-05-24 01:37:15 +0300

    tests: Add deno tests

--- tests/languages/deno/tests.ts
+++ tests/languages/deno/tests.ts
@@ -1,5 +1,6 @@
-import * as appwrite from './main'
-//import * as fs from "fs";
+import * as appwrite from '../../sdks/deno/mod.ts'
+
+// TODO: Correct test typings and remove '// @ts-ignore'
 
 async function start() {
   var response
@@ -16,42 +17,52 @@ async function start() {
   // Foo
 
   response = await foo.get('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   response = await foo.post('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   response = await foo.put('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   response = await foo.patch('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   response = await foo.delete('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   // Bar
 
   response = await bar.get('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   response = await bar.post('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   response = await bar.put('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   response = await bar.patch('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   response = await bar.delete('string', 123, ['string in array'])
+  // @ts-ignore
   console.log(response.result)
 
   response = await general.redirect()
+  // @ts-ignore
   console.log(response.result)
 
-  // response = await general.upload('string', 123, ['string in array'], fs.createReadStream(__dirname + '/../../resources/file.png'));
-  // console.log(response.result);
+  // TODO: Add upload test
 }
 
 start()
