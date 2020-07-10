5bda95730e399ddf3a61779f80b738873ff08b52
Eldad Fux <eldad.fux@gmail.com>
2020-07-10 08:59:56 +0300

    Fixed autoloader

---
+++ src/SDK/Language/Typescript.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Appwrite\SDK\Language;
+
+class Typescript extends JS
+{
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'TypeScript';
+    }
+
+    /**
+     * @return array
+     */
+    public function getFiles()
+    {
+        return [
+            [
+                'scope'         => 'default',
+                'destination'   => 'index.ts',
+                'template'      => '/typescript/index.ts.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => 'tsconfig.json',
+                'template'      => '/typescript/tsconfig.json.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => 'src/client.ts',
+                'template'      => '/typescript/src/client.ts.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => '/src/service.ts',
+                'template'      => '/typescript/src/service.ts.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'service',
+                'destination'   => '/src/services/{{service.name | caseDash}}.ts',
+                'template'      => '/typescript/src/services/service.ts.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => 'README.md',
+                'template'      => '/typescript/README.md.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => 'CHANGELOG.md',
+                'template'      => '/typescript/CHANGELOG.md.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => 'LICENSE',
+                'template'      => '/typescript/LICENSE.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => 'package.json',
+                'template'      => '/typescript/package.json.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'method',
+                'destination'   => 'docs/examples/{{service.name | caseLower}}/{{method.name | caseDash}}.md',
+                'template'      => '/typescript/docs/example.md.twig',
+                'minify'        => false,
+            ],
+        ];
+    }
+
+   /**
+     * @param $type
+     * @return string
+     */
+    public function getTypeName($type)
+    {
+        switch ($type) {
+            case self::TYPE_INTEGER:
+                return 'number';
+            break;
+            case self::TYPE_STRING:
+                return 'string';
+            break;
+            case self::TYPE_FILE:
+                return 'HTMLElement';
+            break;
+            case self::TYPE_BOOLEAN:
+                return 'boolean';
+            break;
+            case self::TYPE_ARRAY:
+            	return 'Array<string>';
+			case self::TYPE_OBJECT:
+				return 'assoc';
+            break;
+        }
+
+        return $type;
+    }
+
+    /**
+     * @param array $param
+     * @return string
+     */
+    public function getParamExample(array $param)
+    {
+        $type       = (isset($param['type'])) ? $param['type'] : '';
+        $example    = (isset($param['example'])) ? $param['example'] : '';
+
+        $output = '';
+
+        if(empty($example) && $example !== 0 && $example !== false) {
+            switch ($type) {
+                case self::TYPE_NUMBER:
+                case self::TYPE_INTEGER:
+                case self::TYPE_BOOLEAN:
+                    $output .= 'null';
+                    break;
+                case self::TYPE_STRING:
+                    $output .= "''";
+                    break;
+                case self::TYPE_ARRAY:
+                    $output .= '[]';
+                    break;
+                case self::TYPE_OBJECT:
+                    $output .= '{}';
+                    break;
+                case self::TYPE_FILE:
+                    $output .= "fs.createReadStream(__dirname + '/file.png'))";
+                    break;
+            }
+        }
+        else {
+            switch ($type) {
+                case self::TYPE_NUMBER:
+                case self::TYPE_INTEGER:
+                case self::TYPE_ARRAY:
+                case self::TYPE_OBJECT:
+                    $output .= $example;
+                    break;
+                case self::TYPE_BOOLEAN:
+                    $output .= ($example) ? 'true' : 'false';
+                    break;
+                case self::TYPE_STRING:
+                    $output .= "'{$example}'";
+                    break;
+                case self::TYPE_FILE:
+                    $output .= "fs.createReadStream(__dirname + '/file.png'))";
+                    break;
+            }
+        }
+
+        return $output;
+    }
+}
