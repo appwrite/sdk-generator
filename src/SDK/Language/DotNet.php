3a9fdc361473639212d617a6287c4916cca74bb1
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-01-07 12:52:27 +0100

    feat(dotnet): add default values to params

--- src/SDK/Language/DotNet.php
+++ src/SDK/Language/DotNet.php
@@ -167,7 +167,52 @@ class DotNet extends Language {
      */
     public function getParamDefault(array $param)
     {
-        return '';
+        $type       = $param['type'] ?? '';
+        $default    = $param['default'] ?? '';
+        $required   = $param['required'] ?? '';
+
+        if($required) {
+            return '';
+        }
+
+        $output = ' = ';
+
+        if(empty($default) && $default !== 0 && $default !== false) {
+            switch ($type) {
+                case self::TYPE_NUMBER:
+                case self::TYPE_INTEGER:
+                case self::TYPE_BOOLEAN:
+                    $output .= 'null';
+                    break;
+                case self::TYPE_STRING:
+                    $output .= '""';
+                    break;
+                case self::TYPE_ARRAY:
+                    $output .= '[]';
+                    break;
+                case self::TYPE_OBJECT:
+                    $output .= '{}';
+                    break;
+            }
+        }
+        else {
+            switch ($type) {
+                case self::TYPE_NUMBER:
+                case self::TYPE_INTEGER:
+                case self::TYPE_ARRAY:
+                case self::TYPE_OBJECT:
+                    $output .= $default;
+                    break;
+                case self::TYPE_BOOLEAN:
+                    $output .= ($default) ? 'true' : 'false';
+                    break;
+                case self::TYPE_STRING:
+                    $output .= "\"{$default}\"";
+                    break;
+            }
+        }
+
+        return $output;
     }
 
     /**
