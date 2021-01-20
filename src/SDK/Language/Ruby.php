5af92ed1fe259f8422ebe82f8ad8206f464ab55e
Damodar Lohani <dlohani48@gmail.com>
2021-01-20 13:22:20 +0545

    fixing new file not in the getFiles list

--- src/SDK/Language/Ruby.php
+++ src/SDK/Language/Ruby.php
@@ -131,6 +131,12 @@ class Ruby extends Language {
                 'minify'        => false,
             ],
             [
+                'scope'         => 'default',
+                'destination'   => 'lib/{{ spec.title | caseDash }}/file.rb',
+                'template'      => '/ruby/lib/container/file.rb.twig',
+                'minify'        => false,
+            ],
+            [
                 'scope'         => 'service',
                 'destination'   => '/lib/{{ spec.title | caseDash}}/services/{{service.name | caseDash}}.rb',
                 'template'      => '/ruby/lib/container/services/service.rb.twig',
