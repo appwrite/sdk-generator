dfeb1a3d68c91e09e3b7d687bd2a93026d1524ea
Eldad Fux <eldad.fux@gmail.com>
2021-02-15 22:47:40 +0200

    Updated PHP 8 tests

--- tests/SDKTest.php
+++ tests/SDKTest.php
@@ -26,7 +26,7 @@ class SDKTest extends TestCase
                 'php-7.2' => 'docker run --rm -v $(pwd):/app -w /app php:7.2-cli-alpine php tests/languages/php/test.php',
                 'php-7.3' => 'docker run --rm -v $(pwd):/app -w /app php:7.3-cli-alpine php tests/languages/php/test.php',
                 'php-7.4' => 'docker run --rm -v $(pwd):/app -w /app php:7.4-cli-alpine php tests/languages/php/test.php',
-                'php-8.0' => 'docker run --rm -v $(pwd):/app -w /app php:8.0.0rc1-cli-alpine php tests/languages/php/test.php',
+                'php-8.0' => 'docker run --rm -v $(pwd):/app -w /app php:8.0-cli-alpine php tests/languages/php/test.php',
             ],
             'supportRedirect' => true,
             'supportUpload' => true,
