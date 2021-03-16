9055d9f4a13367c17ae83fd462ba5858145f721d
Damodar Lohani <dlohani48@gmail.com>
2021-03-16 17:41:09 +0545

    cleanups

--- tests/SDKTest.php
+++ tests/SDKTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests;
 
-use Appwrite\Spec\Swagger2;
 use Appwrite\SDK\SDK;
+use Appwrite\Spec\Swagger2;
 use PHPUnit\Framework\TestCase;
 
 ini_set('display_errors', 1);
@@ -15,7 +15,7 @@ class SDKTest extends TestCase
     /**
      * @var array
      */
-    protected $languages  = [
+    protected $languages = [
         'php' => [
             'class' => 'Appwrite\SDK\Language\PHP',
             'build' => [
@@ -28,8 +28,6 @@ class SDKTest extends TestCase
                 'php-7.4' => 'docker run --rm -v $(pwd):/app -w /app php:7.4-cli-alpine php tests/languages/php/test.php',
                 'php-8.0' => 'docker run --rm -v $(pwd):/app -w /app php:8.0.0rc1-cli-alpine php tests/languages/php/test.php',
             ],
-            'supportRedirect' => true,
-            'supportUpload' => true,
         ],
 
         'cli' => [
@@ -39,14 +37,13 @@ class SDKTest extends TestCase
                 'cat tests/sdks/cli/Dockerfile',
                 'mkdir tests/sdks/cli/files',
                 'cp tests/resources/file.png tests/sdks/cli/files/',
-                'docker build -t cli:latest tests/sdks/cli'
+                'docker build -t cli:latest tests/sdks/cli',
             ],
             'envs' => [
                 'default' => 'php tests/languages/cli/test.php',
             ],
-            'supportUpload' => true,
         ],
-        
+
         'dart' => [
             'class' => 'Appwrite\SDK\Language\Dart',
             'build' => [
@@ -61,9 +58,7 @@ class SDKTest extends TestCase
                 'dart-2.10' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/dart --env PUB_CACHE=vendor google/dart:2.10 dart pub run tests/tests.dart',
                 'dart-2.12-beta' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/dart --env PUB_CACHE=vendor google/dart:2.12-beta dart pub run tests/tests.dart',
             ],
-            'supportRedirect' => true,
-            'supportUpload' => true,
-            'supportException' => true
+            'supportException' => true,
         ],
 
         'java' => [
@@ -76,8 +71,6 @@ class SDKTest extends TestCase
                 'java-11' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/java --env PUB_CACHE=vendor maven:3.6-jdk-11-slim mvn clean install test -q',
                 //'java-14' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/java --env PUB_CACHE=vendor maven:3.6-jdk-14-slim mvn clean install test -q',
             ],
-            'supportRedirect' => false,
-            'supportUpload' => false,
         ],
 
         'dotnet' => [
@@ -90,27 +83,23 @@ class SDKTest extends TestCase
             ],
             'envs' => [
                 'dotnet-5.0' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/dotnet/src/test/ mcr.microsoft.com/dotnet/sdk:5.0-alpine pwsh tests.ps1',
-                'dotnet-3.1' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/dotnet/src/test/ mcr.microsoft.com/dotnet/sdk:3.1-alpine pwsh tests.ps1'
+                'dotnet-3.1' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/dotnet/src/test/ mcr.microsoft.com/dotnet/sdk:3.1-alpine pwsh tests.ps1',
             ],
-            'supportRedirect' => true,
-            'supportUpload' => true,
         ],
 
         'typescript' => [
             'class' => 'Appwrite\SDK\Language\Typescript',
             'build' => [
                 'cp tests/languages/typescript/tests.ts tests/sdks/typescript/tests.ts',
-                'docker run --rm -v $(pwd):/app -w /app/tests/sdks/typescript node:14.5-alpine npm install', //  npm list --depth 0 && 
+                'docker run --rm -v $(pwd):/app -w /app/tests/sdks/typescript node:14.5-alpine npm install', //  npm list --depth 0 &&
                 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/typescript node:14.5-alpine ls node_modules/.bin',
-                'docker run --rm -v $(pwd):/app -w /app/tests/sdks/typescript node:14.5-alpine node_modules/.bin/tsc --lib ES6,DOM tests'
+                'docker run --rm -v $(pwd):/app -w /app/tests/sdks/typescript node:14.5-alpine node_modules/.bin/tsc --lib ES6,DOM tests',
             ],
             'envs' => [
                 'nodejs-14' => 'docker run --rm -v $(pwd):/app -w /app node:14.5-alpine node tests/sdks/typescript/tests.js',
             ],
-            'supportRedirect' => false,
-            'supportUpload' => false,
         ],
-        
+
         'deno' => [
             'class' => 'Appwrite\SDK\Language\Deno',
             'build' => [
@@ -118,14 +107,12 @@ class SDKTest extends TestCase
             'envs' => [
                 'deno-1.1.3' => 'docker run --rm -v $(pwd):/app -w /app hayd/alpine-deno:1.1.3 run --allow-net --allow-read tests/languages/deno/tests.ts', // TODO: use official image when its out
             ],
-            'supportRedirect' => true,
-            'supportUpload' => true,
         ],
 
         'node' => [
             'class' => 'Appwrite\SDK\Language\Node',
             'build' => [
-                'docker run --rm -v $(pwd):/app -w /app/tests/sdks/node node:12.12-alpine npm install'
+                'docker run --rm -v $(pwd):/app -w /app/tests/sdks/node node:12.12-alpine npm install',
             ],
             'envs' => [
                 'nodejs-8' => 'docker run --rm -v $(pwd):/app -w /app node:8.16-alpine node tests/languages/node/test.js',
@@ -133,14 +120,12 @@ class SDKTest extends TestCase
                 'nodejs-12' => 'docker run --rm -v $(pwd):/app -w /app node:12.12-alpine node tests/languages/node/test.js',
                 'nodejs-14' => 'docker run --rm -v $(pwd):/app -w /app node:14.5-alpine node tests/languages/node/test.js',
             ],
-            'supportRedirect' => true,
-            'supportUpload' => true,
         ],
 
         'ruby' => [
             'class' => 'Appwrite\SDK\Language\Ruby',
             'build' => [
-                'docker run --rm -v $(pwd):/app -w /app/tests/sdks/ruby --env GEM_HOME=/app/vendor ruby:2.7-alpine sh -c "apk add git build-base && bundle install"'
+                'docker run --rm -v $(pwd):/app -w /app/tests/sdks/ruby --env GEM_HOME=/app/vendor ruby:2.7-alpine sh -c "apk add git build-base && bundle install"',
             ],
             'envs' => [
                 'ruby-2.7' => 'docker run --rm -v $(pwd):/app -w /app --env GEM_HOME=vendor ruby:2.7-alpine ruby tests/languages/ruby/tests.rb',
@@ -148,8 +133,6 @@ class SDKTest extends TestCase
                 'ruby-2.5' => 'docker run --rm -v $(pwd):/app -w /app --env GEM_HOME=vendor ruby:2.5-alpine ruby tests/languages/ruby/tests.rb',
                 'ruby-2.4' => 'docker run --rm -v $(pwd):/app -w /app --env GEM_HOME=vendor ruby:2.4-alpine ruby tests/languages/ruby/tests.rb',
             ],
-            'supportRedirect' => true,
-            'supportUpload' => true,
         ],
 
         'python' => [
@@ -157,7 +140,7 @@ class SDKTest extends TestCase
             'build' => [
                 'cp tests/languages/python/tests.py tests/sdks/python/test.py',
                 'echo "" > tests/sdks/python/__init__.py',
-                'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor python:3.8 pip install -r tests/sdks/python/requirements.txt --upgrade'
+                'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor python:3.8 pip install -r tests/sdks/python/requirements.txt --upgrade',
             ],
             'envs' => [
                 'python-3.8' => 'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor --env PYTHONPATH=tests/sdks/python/vendor python:3.8-alpine python tests/sdks/python/test.py',
@@ -169,8 +152,6 @@ class SDKTest extends TestCase
                 // 'python-3.2' => 'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor --env PYTHONPATH=tests/sdks/python/vendor python:3.2 python tests/sdks/python/test.py',
                 // 'python-3.1' => 'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor --env PYTHONPATH=tests/sdks/python/vendor python:3.1 python tests/sdks/python/test.py',
             ],
-            'supportRedirect' => true,
-            'supportUpload' => true,
         ],
     ];
 
@@ -196,18 +177,18 @@ class SDKTest extends TestCase
 
         $spec = file_get_contents(realpath(__DIR__ . '/resources/spec.json'));
 
-        if(empty($spec)) {
+        if (empty($spec)) {
             throw new \Exception('Failed to fetch spec from Appwrite server');
         }
 
         $whitelist = ['php', 'cli', 'java', 'node', 'ruby', 'python', 'typescript', 'deno', 'dotnet', 'dart'];
-        $whitelist = ['dart'];
+
         foreach ($this->languages as $language => $options) {
-            if(!empty($whitelist) && !in_array($language, $whitelist)) {
+            if (!empty($whitelist) && !in_array($language, $whitelist)) {
                 continue;
             }
 
-            $sdk  = new SDK(new $options['class'](), new Swagger2($spec));
+            $sdk = new SDK(new $options['class'](), new Swagger2($spec));
 
             $sdk
                 ->setDescription('Repo description goes here')
@@ -234,21 +215,21 @@ class SDKTest extends TestCase
             /**
              * Build SDK
              */
-            if(isset($options['build'])) {
-                
+            if (isset($options['build'])) {
+
                 foreach ($options['build'] as $key => $command) {
                     echo "Building phase #{$key} for {$language} package...\n";
                     echo "Executing: {$command}\n";
-                    
+
                     $output = [];
 
                     ob_end_clean();
-                    var_dump('Build Executing: '.$command);
+                    var_dump('Build Executing: ' . $command);
                     ob_start();
 
                     exec($command, $output);
 
-                    foreach($output as $i => $row) {
+                    foreach ($output as $i => $row) {
                         echo "{$i}. {$row}\n";
                     }
                 }
@@ -261,19 +242,19 @@ class SDKTest extends TestCase
                 echo "Running tests for the {$key} environment...\n";
 
                 $output = [];
-                
+
                 ob_end_clean();
-                var_dump('Env Executing: '.$command);
+                var_dump('Env Executing: ' . $command);
                 ob_start();
-                
+
                 exec($command, $output);
 
-                foreach($output as $i => $row) {
+                foreach ($output as $i => $row) {
                     echo "{$row}\n";
                 }
 
                 $this->assertIsArray($output);
-                $this->assertGreaterThan(10, count($output));
+                $this->assertGreaterThan(11, count($output));
 
                 $this->assertEquals('GET:/v1/mock/tests/foo:passed', $output[0] ?? '');
                 $this->assertEquals('POST:/v1/mock/tests/foo:passed', $output[1] ?? '');
@@ -285,25 +266,15 @@ class SDKTest extends TestCase
                 $this->assertEquals('PUT:/v1/mock/tests/bar:passed', $output[7] ?? '');
                 $this->assertEquals('PATCH:/v1/mock/tests/bar:passed', $output[8] ?? '');
                 $this->assertEquals('DELETE:/v1/mock/tests/bar:passed', $output[9] ?? '');
-                
                 $this->assertEquals('GET:/v1/mock/tests/general/redirect/done:passed', $output[10]);
-                
-                $supportsUpload = $options['supportUpload'];
-                if($supportsUpload) {
-                    $this->assertEquals($output[11], 'POST:/v1/mock/tests/general/upload:passed');
-                }
-                
-                if($options['supportException']) {
-                    if($supportsUpload) {
-                        $this->assertEquals($output[12], 'Mock 400 error');
-                        $this->assertEquals($output[13], 'Server Error');
-                    } else {
-                        $this->assertEquals($output[11], 'Mock 400 error');
-                        $this->assertEquals($output[12], 'Server Error');
-                    }
+                $this->assertEquals($output[11], 'POST:/v1/mock/tests/general/upload:passed');
+
+                if ($options['supportException']) {
+                    $this->assertEquals($output[12], 'Mock 400 error');
+                    $this->assertEquals($output[13], 'Server Error');
                 }
             }
         }
-    
+
     }
 }
