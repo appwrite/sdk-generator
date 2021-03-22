c0bb1093992976887cfe79e2d5f89f31617c9111
Bradley Schofield <ionicisere@gmail.com>
2021-03-22 11:35:42 +0000

    Remove Java from SDKTests

--- tests/SDKTest.php
+++ tests/SDKTest.php
@@ -197,7 +197,7 @@ class SDKTest extends TestCase
             throw new \Exception('Failed to fetch spec from Appwrite server');
         }
 
-        $whitelist = ['php', 'cli', 'java', 'node', 'ruby', 'python', 'typescript', 'deno', 'dotnet', 'dart', 'rust'];
+        $whitelist = ['php', 'cli', 'node', 'ruby', 'python', 'typescript', 'deno', 'dotnet', 'dart', 'rust'];
 
         foreach ($this->languages as $language => $options) {
             if (!empty($whitelist) && !in_array($language, $whitelist)) {
