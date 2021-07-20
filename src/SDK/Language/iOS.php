eef405fd876159dbb0f33237640f84b96d41166f
Jake Barnby <jake@jakebarnby.com>
2021-07-20 23:19:28 +1200

    Add iOS language file

---
+++ src/SDK/Language/iOS.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Appwrite\SDK\Language;
+
+class iOS extends Swift {
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'iOS';
+    }
+
+    public function getFiles()
+    {
+        return array_merge(parent::getFiles(), [
+            [
+                'scope'         => 'default',
+                'destination'   => '/Sources/{{ spec.title | caseUcfirst}}/WebAuthComponent.swift',
+                'template'      => '/swift/Sources/WebAuthComponent.swift.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => '/Sources/{{ spec.title | caseUcfirst}}/Error.swift',
+                'template'      => '/swift/Sources/Error.swift.twig',
+                'minify'        => false,
+            ],
+        ]);
+    }
+}
