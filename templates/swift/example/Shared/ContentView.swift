f2ba34de82925ae7b4c9387a68b51665e813030a
Jake Barnby <jakeb994@gmail.com>
2021-08-03 21:37:05 +1200

    Add Swift example base project

---
+++ templates/swift/example/Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jake Barnby on 3/08/21.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ templates/swift/example/Shared/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Jake Barnby on 3/08/21.
+//
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
