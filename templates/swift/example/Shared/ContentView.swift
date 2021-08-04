e125699ec97aed84b2e97a95f858a0d750689369
Jake Barnby <jakeb994@gmail.com>
2021-08-04 23:46:37 +1200

    WIP add SDK interaction to example app with oauth

--- templates/swift/example/Shared/ContentView.swift
+++ templates/swift/example/Shared/ContentView.swift
@@ -7,17 +7,109 @@
 //
 
 import SwiftUI
+import Appwrite
+
+let client = Client()
+    .setEndpoint("https://demo.appwrite.io/v1")
+    .setProject("60f6a0d6e2a52")
 
 struct ContentView: View {
+
+    @State var response: String = ""
+    @ObservedObject var keyboard: Keyboard = .init()
+
+    let account = Account(client: client)
+
     var body: some View {
-        VStack {
-            Button("")
-        }
+        VStack(spacing: 8) {
+
+            TextEditor(text: $response)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+                .padding(.top, 100)
+                .padding(.bottom, keyboard.height)
+                .edgesIgnoringSafeArea(keyboard.height > 0 ? .bottom : [])
+
+            Divider()
+
+            HStack {
+
+                Button("Login") {
+                    do {
+                        try account.createSession("test@test.test", "password") { result in
+                            switch result {
+                            case .failure(let error): self.response = error.localizedDescription
+                            case .success(var response): self.response = response.body!.readString()
+                            }
+                        }
+                    } catch let error {
+                        print(error)
+                    }
+                }
+
+                Button("Login with Facebook") {
+                    do {
+                        try account.createOAuth2Session(
+                            "facebook",
+                            "https://demo.appwrite.io/auth/oauth2/success",
+                            "https://demo.appwrite.io/auth/oauth2/success"
+                        ) { result in
+                            switch result {
+                            case .failure: self.response = "false"
+                            case .success(let response): self.response = response.description
+                            }
+                        }
+                    } catch let error {
+                        print(error)
+                    }
+                }
+
+                Button("Register") {
+                    do {
+                        try account.create("test@test.test", "password") { result in
+                            switch result {
+                            case .failure(let error): self.response = error.localizedDescription
+                            case .success(var response): self.response = response.body!.readString()
+                            }
+                        }
+                    } catch let error {
+                        print(error)
+                    }
+                }
+
+                Button("Realtime") {
+
+                }
+            }
+        }.registerOauthHandler()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+import NIO
+import Combine
+
+extension ByteBuffer {
+
+    ///
+    /// Read an entire byte array into a string
+    ///
+    /// - Returns:
+    public mutating func readString() -> String {
+//        readableBytesView.publisher.receive(subscriber:  Subscribers.Sink<UInt8, Never>(receiveCompletion: { completion in
+//
+//        }, receiveValue: { bufferView in
+//
+//        }))
+        readString(length: readableBytes) ?? ""
+    }
+
+    public mutating func readJSONDecodable<T : Decodable>(type: T.Type) throws -> T? {
+        try readJSONDecodable(type, length: readableBytes) ?? nil
     }
 }
