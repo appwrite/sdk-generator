811b8d1b2850462b83633acba6473a8d8af3ac88
Jake Barnby <jakeb994@gmail.com>
2021-10-12 20:24:36 +1300

    Update linux package info to read version file

--- templates/swift/Sources/PackageInfo/Linux/PackageInfo+Linux.swift
+++ templates/swift/Sources/PackageInfo/Linux/PackageInfo+Linux.swift
@@ -10,27 +10,22 @@ import Foundation
 extension PackageInfo {
 
     public static func getLinuxPackage() -> PackageInfo {
-        let versionJson = getVersionJson()
-        
+        let version = getVersionJson()
+
         return PackageInfo(
-            appName: "",
-            version: "",
-            buildNumber: "",
+            appName: version["app_name"] as! String,
+            version: version["version"] as! String,
+            buildNumber: version["build_number"] as! String,
             packageName: "",
             buildSignature: ""
         )
     }
-    
-    private static func getVersionJson() -> String {
+
+    private static func getVersionJson() -> [String: Any] {
         let exePath = URL(fileURLWithPath: "/proc/self/exe").resolvingSymlinksInPath()
         let appPath = exePath.deletingLastPathComponent()
-        let resolved = appPath.appendingPathComponent("Package.resolved")
-        let contents = try! String(contentsOfFile: resolved.absoluteString)
-        return ""
-        // Package.resolved path?
-//        final appPath = path.dirname(exePath);
-//        final assetPath = path.join(appPath, 'data', 'flutter_assets');
-//        final versionPath = path.join(assetPath, 'version.json');
-//        return jsonDecode(await File(versionPath).readAsString());
+        let jsonPath = appPath.appendingPathComponent("version.json")
+        return try! JSONSerialization
+            .jsonObject(with: Data(contentsOf: jsonPath)) as! [String: Any]
     }
 }
