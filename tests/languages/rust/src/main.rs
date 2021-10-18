820ed3cf82931689c0ebdba9b6abf4922f72ac46
Bradley Schofield <ionicisere@gmail.com>
2021-10-18 15:56:31 +0100

    Fix tests for response models
    + Implemented Display trait on custom structs
    + Got tests working with the response models

--- tests/languages/rust/src/main.rs
+++ tests/languages/rust/src/main.rs
@@ -9,23 +9,6 @@ struct DefaultResponse {
   message: Option<String>
 }
 
-fn parse_response(data: Response) -> String {
-    let response:DefaultResponse = match data.json() {
-      Ok(value) => value,
-      Err(err) => {
-        return err.to_string()
-      }
-    };
-
-    match response.result {
-      Some(data) => return data,
-      None => return match response.message {
-        Some(value) => value,
-        None => "Couldn't parse error.".to_string()
-      }
-    }
-  }
-
 fn main() {
     let client = packageName::client::Client::new();
 
@@ -37,37 +20,36 @@ fn main() {
 
     // Foo
 
-    println!("{}", parse_response(foo.get("string", 123, &["string in array"]).unwrap()));
-    println!("{}", parse_response(foo.post("string", 123, &["string in array"]).unwrap()));
-    println!("{}", parse_response(foo.put("string", 123, &["string in array"]).unwrap()));
-    println!("{}", parse_response(foo.patch("string", 123, &["string in array"]).unwrap()));
-    println!("{}", parse_response(foo.delete("string", 123, &["string in array"]).unwrap()));
+    println!("{}", foo.get("string", 123, &["string in array"]).unwrap());
+    println!("{}", foo.post("string", 123, &["string in array"]).unwrap());
+    println!("{}", foo.put("string", 123, &["string in array"]).unwrap());
+    println!("{}", foo.patch("string", 123, &["string in array"]).unwrap());
+    println!("{}", foo.delete("string", 123, &["string in array"]).unwrap());
 
     // Bar
 
-    println!("{}", parse_response(bar.get("string", 123, &["string in array"]).unwrap()));
-    println!("{}", parse_response(bar.post("string", 123, &["string in array"]).unwrap()));
-    println!("{}", parse_response(bar.put("string", 123, &["string in array"]).unwrap()));
-    println!("{}", parse_response(bar.patch("string", 123, &["string in array"]).unwrap()));
-    println!("{}", parse_response(bar.delete("string", 123, &["string in array"]).unwrap()));
+    println!("{}", bar.get("string", 123, &["string in array"]).unwrap());
+    println!("{}", bar.post("string", 123, &["string in array"]).unwrap());
+    println!("{}", bar.put("string", 123, &["string in array"]).unwrap());
+    println!("{}", bar.patch("string", 123, &["string in array"]).unwrap());
+    println!("{}", bar.delete("string", 123, &["string in array"]).unwrap());
 
     // General
-
-    println!("{}", parse_response(general.redirect().unwrap()));
-    println!("{}", parse_response(general.upload("string", 123, &["string in array"], PathBuf::from("../../../resources/file.png")).unwrap()));
+    println!("{}", general.redirect().unwrap()["result"].as_str().unwrap());
+    println!("{}", general.upload("string", 123, &["string in array"], PathBuf::from("../../../resources/file.png")).unwrap());
 
     match general.error400() {
-      Ok(data) => println!("{}", data.text().unwrap()),
+      Ok(data) => println!("{}", data),
       Err(err) => println!("{}", err.message)
     }
 
     match general.error500() {
-      Ok(data) => println!("{}", data.text().unwrap()),
+      Ok(data) => println!("{}", data),
       Err(err) => println!("{}", err.message)
     }
 
     match general.error502() {
-      Ok(data) => println!("{}", data.text().unwrap()),
+      Ok(data) => println!("{}", data),
       Err(err) => println!("{}", err.message)
     }
 }
