9bd3936631b9843eb00c665eb579aba175e2d88c
Jake Barnby <jakeb994@gmail.com>
2021-10-22 01:05:29 +1300

    Update test for response models

--- tests/languages/android/ServiceTest.kt
+++ tests/languages/android/ServiceTest.kt
@@ -6,6 +6,8 @@ import com.google.gson.Gson
 import io.appwrite.exceptions.AppwriteException
 import io.appwrite.extensions.fromJson
 import io.appwrite.extensions.toJson
+import io.appwrite.models.Mock
+import io.appwrite.models.Error
 import io.appwrite.services.Bar
 import io.appwrite.services.Foo
 import io.appwrite.services.General
@@ -68,37 +70,37 @@ class ServiceTest {
         }
 
         runBlocking {
-            var response: Response
+            var mock: Mock
             // Foo Tests
-            response = foo.get("string", 123, listOf("string in array"))
-            printResponse(response)
-            response = foo.post("string", 123, listOf("string in array"))
-            printResponse(response)
-            response = foo.put("string", 123, listOf("string in array"))
-            printResponse(response)
-            response = foo.patch("string", 123, listOf("string in array"))
-            printResponse(response)
-            response = foo.delete("string", 123, listOf("string in array"))
-            printResponse(response)
+            mock = foo.get("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
+            mock = foo.post("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
+            mock = foo.put("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
+            mock = foo.patch("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
+            mock = foo.delete("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
 
             // Bar Tests
-            response = bar.get("string", 123, listOf("string in array"))
-            printResponse(response)
-            response = bar.post("string", 123, listOf("string in array"))
-            printResponse(response)
-            response = bar.put("string", 123, listOf("string in array"))
-            printResponse(response)
-            response = bar.patch("string", 123, listOf("string in array"))
-            printResponse(response)
-            response = bar.delete("string", 123, listOf("string in array"))
-            printResponse(response)
+            mock = bar.get("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
+            mock = bar.post("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
+            mock = bar.put("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
+            mock = bar.patch("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
+            mock = bar.delete("string", 123, listOf("string in array"))
+            writeToFile(mock.result)
 
             // General Tests
-            response = general.redirect()
-            printResponse(response)
+            val result = general.redirect()
+            writeToFile((result as Map<String, Any>)["result"] as String)
 
-            response = general.upload("string", 123, listOf("string in array"), File("../../../resources/file.png"))
-            printResponse(response)
+            mock = general.upload("string", 123, listOf("string in array"), File("../../../resources/file.png"))
+            writeToFile(mock.result)
 
             try {
                 general.error400()
@@ -121,17 +123,6 @@ class ServiceTest {
             delay(5000)
             writeToFile(realtimeResponse)
         }
-    }
-
-    @Throws(IOException::class)
-    private fun printResponse(response: Response) {
-        // Store the outputs in a file and
-        val gson = Gson()
-        val map = gson.fromJson<Map<*, *>>(
-            response.body!!.string(),
-            MutableMap::class.java
-        )
-        writeToFile(map["result"] as String)
     }
 
     private fun writeToFile(string: String?) {
