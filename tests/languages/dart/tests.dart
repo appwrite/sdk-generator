89514d22e35d92e882a65cf19f6195de485d4958
Damodar Lohani <dlohani48@gmail.com>
2021-10-25 10:29:25 +0545

    fix dart tests

--- tests/languages/dart/tests.dart
+++ tests/languages/dart/tests.dart
@@ -1,4 +1,5 @@
 import '../lib/packageName.dart';
+import '../lib/models.dart';
 
 void main() async {
   Client client = Client();
@@ -12,49 +13,49 @@ void main() async {
   print('\nTest Started');
   
   // Foo Tests
-  Response response;
+  Mock response;
   response = await foo.get(x: 'string', y: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   response = await foo.post(x: 'string', y: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   response = await foo.put(x: 'string', y: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   response = await foo.patch(x: 'string', y: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   response = await foo.delete(x: 'string', y: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   // Bar Tests
 
   response = await bar.get(xrequired: 'string', xdefault: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   response = await bar.post(xrequired: 'string', xdefault: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   response = await bar.put(xrequired: 'string', xdefault: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   response = await bar.patch(xrequired: 'string', xdefault: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   response = await bar.delete(xrequired: 'string', xdefault: 123, z: ['string in array']);
-  print(response.data['result']);
+  print(response.result);
 
   // General Tests
 
   response = await general.redirect();
-  print(response.data['result']);
+  print(response['result']);
 
   final file = await MultipartFile.fromPath('file', '../../resources/file.png',
       filename: 'file.png');
   response = await general.upload(
       x: 'string', y: 123, z: ['string in array'], file: file);
-  print(response.data['result']);
+  print(response.result);
 
   try {
     await general.error400();
@@ -75,8 +76,8 @@ void main() async {
   }
 
   // response = await general.setCookie();
-  // print(response.data['result']);
+  // print(response.result);
 
   // response = await general.getCookie();
-  // print(response.data['result']);
+  // print(response.result);
 }
