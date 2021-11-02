54dd8b79f669e89bbc1f71fef40c568a2c7069e6
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2021-11-02 16:00:31 +0545

    Update tests/SDKTest.php
    Co-authored-by: Jake Barnby <jakeb994@gmail.com>

--- tests/SDKTest.php
+++ tests/SDKTest.php
@@ -331,7 +331,6 @@ class SDKTest extends TestCase
         }
 
         $whitelist = ['php', 'cli', 'node', 'ruby', 'python', 'deno', 'dotnet', 'dart', 'flutter', 'web', 'android', 'kotlin', 'swift-server', 'swift-client'];
-        $whitelist = ['flutter'];
 
         foreach ($this->languages as $language => $options) {
             if (!empty($whitelist) && !in_array($language, $whitelist)) {
