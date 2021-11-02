1391d8ea318ed44508fe43ffcb1ce9f5d524a6d6
Damodar Lohani <dlohani48@gmail.com>
2021-11-02 16:04:47 +0545

    fix test

--- tests/SDKTest.php
+++ tests/SDKTest.php
@@ -55,7 +55,6 @@ class SDKTest extends TestCase
                 'php-7.4' => 'docker run --rm -v $(pwd):/app -w /app php:7.4-cli-alpine php tests/languages/php/test.php',
                 'php-8.0' => 'docker run --rm -v $(pwd):/app -w /app php:8.0-cli-alpine php tests/languages/php/test.php',
             ],
-            'supportException' => true,
             'expectedOutput' => [
                 ...FOO_RESPONSES,
                 ...BAR_RESPONSES,
@@ -413,8 +412,8 @@ class SDKTest extends TestCase
 
                 $this->assertGreaterThanOrEqual(count($options['expectedOutput']), count($output));
 
-                foreach ($output as $i => $row) {
-                    $this->assertEquals($options['expectedOutput'][$i], $row);
+                foreach ($options['expectedOutput'] as $i => $row) {
+                    $this->assertEquals($output[$i], $row);
                 }
             }
         }
