c2cac3f6e2d45b846e4aae7a9c23f0ee30fcb9ea
Damodar Lohani <dlohani48@gmail.com>
2022-02-15 11:17:46 +0000

    add node cli test

---
+++ tests/NodeCLITest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests;
+
+class NodeTest extends Base
+{
+    protected string $language = 'node-cli';
+    protected string $class = 'Appwrite\SDK\Language\NodeCLI';
+    protected array $build = [
+        'docker run --rm -v $(pwd):/app -w /app/tests/sdks/node-cli node:16-alpine npm install',
+        'cp tests/languages/node-cli/test.js tests/sdks/node-cli/test.js'
+    ];
+    protected array $envs = [
+        'nodejs-14' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/node-cli node:14-alpine node test.js',
+        'nodejs-16' => 'docker run --rm -v $(pwd):/app -w /app/tests/sdks/node-cli node:16-alpine node test.js',
+    ];
+    protected array $expectedOutput = [
+        ...Base::FOO_RESPONSES,
+        ...Base::BAR_RESPONSES,
+        ...Base::GENERAL_RESPONSES,
+        ...Base::LARGE_FILE_RESPONSES,
+    ];
+}
