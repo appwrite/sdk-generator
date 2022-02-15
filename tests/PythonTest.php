b977fcf357a267f41299539ac9095aa7bbcc2600
Damodar Lohani <dlohani48@gmail.com>
2022-02-15 11:09:40 +0000

    account for large file response

--- tests/PythonTest.php
+++ tests/PythonTest.php
@@ -21,6 +21,7 @@ class PythonTest extends Base
         ...Base::FOO_RESPONSES,
         ...Base::BAR_RESPONSES,
         ...Base::GENERAL_RESPONSES,
+        ...Base::LARGE_FILE_RESPONSES,
         ...Base::EXCEPTION_RESPONSES,
     ];
 }
