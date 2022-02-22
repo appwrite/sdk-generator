a608a385dd5cf54a7e8787ca7f70e0367f05d577
wess <wcope@me.com>
2022-02-22 11:12:49 -0500

    Adds brew formula
    Updates readme file with temp brew instructions

---
+++ templates/cli/Formula/appwrite.rb
@@ -0,0 +1,19 @@
+require "language/node"
+
+class Appwrite < Formula
+  desc "CLI is a Node based command-line tool for Appwrite API"
+  homepage "https://appwrite.io"
+  license "BSD-3-Clause"
+  head "https://github.com/appwrite/sdk-for-node-cli.git", branch: "main"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "true"
+  end
+end
