89751a1d9f7a3c5cfa9e7542d5dae03ed5235db0
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-04-27 16:47:44 +0200

    fix: update to latest master

--- tests/WebChromiumTest.php
+++ tests/WebChromiumTest.php
@@ -10,11 +10,11 @@ class WebChromiumTest extends Base
         'cp tests/languages/web/tests.js tests/sdks/web/tests.js',
         'cp tests/languages/web/node.js tests/sdks/web/node.js',
         'cp tests/languages/web/index.html tests/sdks/web/index.html',
-        'docker run --rm -v $(pwd):/app -w /app/tests/sdks/web mcr.microsoft.com/playwright:v1.15.0-focal npm install', //  npm list --depth 0 &&
-        'docker run --rm -v $(pwd):/app -w /app/tests/sdks/web mcr.microsoft.com/playwright:v1.15.0-focal npm run build',
+        'docker run --rm -v $(pwd):/app -w /app/tests/sdks/web mcr.microsoft.com/playwright:v1.21.0-focal npm install',
+        'docker run --rm -v $(pwd):/app -w /app/tests/sdks/web mcr.microsoft.com/playwright:v1.21.0-focal npm run build',
     ];
     protected string $command =
-        'docker run --rm -v $(pwd):/app -e BROWSER=chromium -w /app/tests/sdks/web mcr.microsoft.com/playwright:v1.15.0-focal node tests.js';
+        'docker run --rm -v $(pwd):/app -e BROWSER=chromium -w /app/tests/sdks/web mcr.microsoft.com/playwright:v1.21.0-focal node tests.js';
 
     protected array $expectedOutput = [
         ...Base::FOO_RESPONSES,
