3a9c6242c40a80ff01954034fa3de925cdfbafc7
Bradley Schofield <ionicisere@gmail.com>
2022-05-03 14:11:11 +0100

    Add beta and nightly tests

--- tests/RustBetaTest.php
+++ tests/RustBetaTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests;
 
-class RustTest extends Base
+class RustBetaTest extends Base
 {
     protected string $language = 'rust';
     protected string $class = 'Appwrite\SDK\Language\Rust';
     protected array $build = [
         'cp -r tests/languages/rust/ tests/sdks/rust/tests/'
     ];
-    protected string $command = 'docker run --network host --rm -v $(pwd):/app -w /app/tests/sdks/rust/tests rust:latest cargo run';
+    protected string $command = 'docker run --network host --rm -v $(pwd):/app -w /app/tests/sdks/rust/tests rust:latest "rustup install beta && rustup default beta && cargo run"';
 
     protected array $expectedOutput = [
         ...Base::FOO_RESPONSES,
--- tests/RustNightlyTest.php
+++ tests/RustNightlyTest.php
@@ -2,14 +2,14 @@
 
 namespace Tests;
 
-class RustTest extends Base
+class RustNightlyTest extends Base
 {
     protected string $language = 'rust';
     protected string $class = 'Appwrite\SDK\Language\Rust';
     protected array $build = [
         'cp -r tests/languages/rust/ tests/sdks/rust/tests/'
     ];
-    protected string $command = 'docker run --network host --rm -v $(pwd):/app -w /app/tests/sdks/rust/tests rust:latest cargo run';
+    protected string $command = 'docker run --network host --rm -v $(pwd):/app -w /app/tests/sdks/rust/tests rust:latest "rustup install nightly && rustup default nightly && cargo run"';
 
     protected array $expectedOutput = [
         ...Base::FOO_RESPONSES,
