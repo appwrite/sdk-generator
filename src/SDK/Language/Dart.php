f0c7c4c7a3c95b1596a0ba79901f4607dca81e86
Damodar Lohani <dlohani48@gmail.com>
2022-05-25 07:27:28 +0000

    chunked upload for dart html platforms

--- src/SDK/Language/Dart.php
+++ src/SDK/Language/Dart.php
@@ -428,6 +428,12 @@ class Dart extends Language {
             ],
             [
                 'scope'         => 'default',
+                'destination'   => 'lib/src/chunked_upload_browser.dart',
+                'template'      => 'dart/lib/src/chunked_upload_browser.dart.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
                 'destination'   => 'lib/src/chunked_upload_stub.dart',
                 'template'      => 'dart/lib/src/chunked_upload_stub.dart.twig',
                 'minify'        => false,
