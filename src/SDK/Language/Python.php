133b8aa82ec22f68162c6a210309a4ea68beac65
Damodar Lohani <dlohani48@gmail.com>
2022-08-15 09:57:57 +0000

    python: Role and ID helpers

--- src/SDK/Language/Python.php
+++ src/SDK/Language/Python.php
@@ -163,6 +163,18 @@ class Python extends Language {
             ],
             [
                 'scope'         => 'default',
+                'destination'   => '{{ spec.title | caseSnake}}/role.py',
+                'template'      => 'python/package/role.py.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
+                'destination'   => '{{ spec.title | caseSnake}}/id.py',
+                'template'      => 'python/package/id.py.twig',
+                'minify'        => false,
+            ],
+            [
+                'scope'         => 'default',
                 'destination'   => '{{ spec.title | caseSnake}}/query.py',
                 'template'      => 'python/package/query.py.twig',
                 'minify'        => false,
