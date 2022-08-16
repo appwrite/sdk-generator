d1e1aa1392806bf2c7adabcef828a217dbe50858
Damodar Lohani <dlohani48@gmail.com>
2022-08-16 08:08:21 +0000

    flutter and dart query helper update

--- tests/languages/flutter/tests.dart
+++ tests/languages/flutter/tests.dart
@@ -114,6 +114,12 @@ void main() async {
   print(Query.lesser('releasedYear', 1990));
   print(Query.greater('releasedYear', [1990, 1999]));
   print(Query.search('name', 'john'));
+  print(Query.orderAsc("title"));
+  print(Query.orderDesc("title"));
+  print(Query.cursorAfter("my_movie_id"));
+  print(Query.cursorBefore("my_movie_id"));
+  print(Query.limit(50));
+  print(Query.offset(20));
 
   // Permission & Role helper tests
   print(Permission.read(Role.any()));
