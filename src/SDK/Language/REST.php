48445da3bf555d360bdc3fa822c9ddcb2b552ee2
Jake Barnby <jakeb994@gmail.com>
2022-10-26 16:38:59 +1300

    Fix extension

--- src/SDK/Language/REST.php
+++ src/SDK/Language/REST.php
@@ -141,7 +141,7 @@ e8 ee 55 94 29 e7 94 89 19 26 28 01 26 29 3f 16...';
         return [
           [
             'scope'         => 'method',
-            'destination'   => 'docs/examples/{{service.name | caseLower}}/{{method.name | caseDash}}',
+            'destination'   => 'docs/examples/{{service.name | caseLower}}/{{method.name | caseDash}}.md',
             'template'      => '/http/docs/example.md.twig',
           ],
         ];
