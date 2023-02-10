4d64dcb745a21b25634b9c4db039f464fc281733
Matej Bačo <matejbaco2000@gmail.com>
2023-02-10 09:00:30 +0000

    Update deno version

--- tests/Deno1171Test.php
+++ tests/Deno1171Test.php
@@ -13,7 +13,7 @@ class Deno1171Test extends Base
     protected string $class = 'Appwrite\SDK\Language\Deno';
     protected array $build = [];
     protected string $command =
-        'docker run --rm -v $(pwd):/app -w /app denoland/deno:alpine-1.17.1 run --allow-net --allow-read tests/languages/deno/tests.ts';
+        'docker run --rm -v $(pwd):/app -w /app denoland/deno:alpine-1.30.3 run --allow-net --allow-read tests/languages/deno/tests.ts';
 
     protected array $expectedOutput = [
         ...Base::FOO_RESPONSES,
