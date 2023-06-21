c51727d4639649fab2131356d7c8dd068a9fc28e
Damodar Lohani <dlohani48@gmail.com>
2023-06-21 00:34:10 +0000

    realtime response connected test

--- src/SDK/Language/Flutter.php
+++ src/SDK/Language/Flutter.php
@@ -282,6 +282,11 @@ class Flutter extends Dart
             ],
             [
                 'scope'         => 'default',
+                'destination'   => '/test/src/realtime_response_connected_test.dart',
+                'template'      => 'flutter/test/src/realtime_response_connected_test.dart.twig',
+            ],
+            [
+                'scope'         => 'default',
                 'destination'   => '/test/src/enums_test.dart',
                 'template'      => 'dart/test/src/enums_test.dart.twig',
             ],
