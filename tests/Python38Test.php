904c35dff0b377a7acdb15e1e8d23b069856c90d
Christy Jacob <christyjacob4@gmail.com>
2023-07-10 16:01:28 +0000

    feat: update requests version and fix ci

--- tests/Python38Test.php
+++ tests/Python38Test.php
@@ -14,7 +14,7 @@ class Python38Test extends Base
     protected array $build = [
         'cp tests/languages/python/tests.py tests/sdks/python/test.py',
         'echo "" > tests/sdks/python/__init__.py',
-        'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor python:3.8 pip install -r tests/sdks/python/requirements.txt --upgrade',
+        'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor python:3.8-alpine pip install -r tests/sdks/python/requirements.txt --upgrade',
     ];
     protected string $command =
         'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor --env PYTHONPATH=tests/sdks/python/vendor python:3.8-alpine python tests/sdks/python/test.py';
