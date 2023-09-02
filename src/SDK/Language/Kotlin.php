37ec26e2d4067e067b9ee3e57bf98519bf6aa910
Jake Barnby <jakeb994@gmail.com>
2023-09-01 22:17:05 -0400

    Fix wrong key used when fetching list sub-type

--- src/SDK/Language/Kotlin.php
+++ src/SDK/Language/Kotlin.php
@@ -117,8 +117,8 @@ class Kotlin extends Language
             case self::TYPE_BOOLEAN:
                 return 'Boolean';
             case self::TYPE_ARRAY:
-                if (!empty($parameter['array']['type'])) {
-                    return 'List<' . $this->getTypeName($parameter['array']) . '>';
+                if (!empty($parameter['items']['type'])) {
+                    return 'List<' . $this->getTypeName($parameter['items']) . '>';
                 }
                 return 'List<Any>';
             case self::TYPE_OBJECT:
