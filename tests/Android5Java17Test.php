ef5e497e0a19c935670ef6ac4681c866a38df502
Jake Barnby <jakeb994@gmail.com>
2024-01-29 16:58:53 +1300

    Update tests for new gradle + JDK version

--- tests/Android5Java17Test.php
+++ tests/Android5Java17Test.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-class Android11Java8Test extends Base
+class Android5Java17Test extends Base
 {
     protected string $sdkName = 'android';
     protected string $sdkPlatform = 'client';
@@ -17,7 +17,7 @@ class Android11Java8Test extends Base
         'chmod +x tests/sdks/android/gradlew',
     ];
     protected string $command =
-        'docker run --network="mockapi" --rm -v $(pwd):/app -w /app/tests/sdks/android alvrme/alpine-android:android-30-jdk8 sh -c "./gradlew :library:testReleaseUnitTest --stacktrace -q && cat library/result.txt"';
+        'docker run --network="mockapi" --rm -v $(pwd):/app -w /app/tests/sdks/android alvrme/alpine-android:android-21-jdk17 sh -c "./gradlew :library:testReleaseUnitTest --stacktrace -q && cat library/result.txt"';
 
     protected array $expectedOutput = [
         ...Base::FOO_RESPONSES,
