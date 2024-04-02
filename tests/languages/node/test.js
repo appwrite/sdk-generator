45919f049feea6322ae8560c3fbbbd0dac8d3f36
loks0n <22452787+loks0n@users.noreply.github.com>
2024-04-02 16:48:48 +0100

    Revert "chore: remove esm build"
    This reverts commit 29e7d9393485a47df316e72892a16d650967e371.

--- tests/languages/node/test.js
+++ tests/languages/node/test.js
@@ -18,6 +18,7 @@ async function start() {
     // Init SDK
     const client = new Client()
         .addHeader("Origin", "http://localhost")
+        .setSelfSigned(true);
 
     const foo = new Foo(client);
     const bar = new Bar(client);
