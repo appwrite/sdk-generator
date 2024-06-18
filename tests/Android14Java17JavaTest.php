acbcb0527bc949cdfb58dfe4afc488faeed6b33d
Jake Barnby <jakeb994@gmail.com>
2024-06-18 12:35:22 +1200

    Add java specific test

---
+++ tests/Android14Java17JavaTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests;
+
+class Android14Java17JavaTest extends Base
+{
+    protected string $sdkName = 'android';
+    protected string $sdkPlatform = 'client';
+    protected string $sdkLanguage = 'android';
+    protected string $version = '0.0.1';
+
+    protected string $language = 'android';
+    protected string $class = 'Appwrite\SDK\Language\Android';
+    protected array $build = [
+        'mkdir -p tests/sdks/android/library/src/test/java',
+        'cp tests/languages/android/Tests.java tests/sdks/android/library/src/test/java/Tests.java',
+        'chmod +x tests/sdks/android/gradlew',
+    ];
+    protected string $command =
+        'docker run --network="mockapi" --rm -v $(pwd):/app -w /app/tests/sdks/android alvrme/alpine-android:android-34-jdk17 sh -c "./gradlew :library:testReleaseUnitTest --stacktrace -q && cat library/result.txt"';
+
+    protected array $expectedOutput = [
+        ...Base::FOO_RESPONSES,
+        ...Base::BAR_RESPONSES,
+        ...Base::GENERAL_RESPONSES,
+        ...Base::UPLOAD_RESPONSES,
+        ...Base::EXCEPTION_RESPONSES,
+        ...Base::REALTIME_RESPONSES,
+        // ...Base::COOKIE_RESPONSES,
+        ...Base::QUERY_HELPER_RESPONSES,
+        ...Base::PERMISSION_HELPER_RESPONSES,
+        ...Base::ID_HELPER_RESPONSES
+    ];
+}
