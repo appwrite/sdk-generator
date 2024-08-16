203bb646d5c92a6ae4621b210fa5e7b3b7fdc06d
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-08-16 14:28:00 -0400

    feat(android): handle multipart data in function executions

--- tests/languages/android/Tests.kt
+++ tests/languages/android/Tests.kt
@@ -12,7 +12,7 @@ import io.appwrite.enums.MockType
 import io.appwrite.extensions.fromJson
 import io.appwrite.extensions.toJson
 import io.appwrite.models.Error
-import io.appwrite.models.InputFile
+import io.appwrite.models.Payload
 import io.appwrite.models.Mock
 import io.appwrite.services.Bar
 import io.appwrite.services.Foo
@@ -106,14 +106,14 @@ class ServiceTest {
             writeToFile((result as Map<String, Any>)["result"] as String)
 
             try {
-                mock = general.upload("string", 123, listOf("string in array"), InputFile.fromPath("../../../resources/file.png"))
+                mock = general.upload("string", 123, listOf("string in array"), Payload.fromPath("../../../resources/file.png"))
                 writeToFile(mock.result)
             } catch (ex: Exception) {
                 writeToFile(ex.toString())
             }
 
             try {
-                mock = general.upload("string", 123, listOf("string in array"), InputFile.fromPath("../../../resources/large_file.mp4"))
+                mock = general.upload("string", 123, listOf("string in array"), Payload.fromPath("../../../resources/large_file.mp4"))
                 writeToFile(mock.result)
             } catch (ex: Exception) {
                 writeToFile(ex.toString())
@@ -121,7 +121,7 @@ class ServiceTest {
 
             try {
                 var bytes = File("../../../resources/file.png").readBytes()
-                mock = general.upload("string", 123, listOf("string in array"), InputFile.fromBytes(bytes, "file.png", "image/png"))
+                mock = general.upload("string", 123, listOf("string in array"), Payload.fromBinary(bytes, "file.png", "image/png"))
                 writeToFile(mock.result)
             } catch (ex: Exception) {
                 writeToFile(ex.toString())
@@ -129,7 +129,7 @@ class ServiceTest {
 
             try {
                 var bytes = File("../../../resources/large_file.mp4").readBytes()
-                mock = general.upload("string", 123, listOf("string in array"), InputFile.fromBytes(bytes, "large_file.mp4", "video/mp4"))
+                mock = general.upload("string", 123, listOf("string in array"), Payload.fromBinary(bytes, "large_file.mp4", "video/mp4"))
                 writeToFile(mock.result)
             } catch (ex: Exception) {
                 writeToFile(ex.toString())
