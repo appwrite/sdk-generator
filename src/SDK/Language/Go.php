08130498de1c89daebb4009509193fd1587b991f
Binyamin Yawitz <316103+byawitz@users.noreply.github.com>
2024-08-19 12:32:43 -0400

    fix(go): Collection attributes to `[]any`

--- src/SDK/Language/Go.php
+++ src/SDK/Language/Go.php
@@ -138,6 +138,9 @@ class Go extends Language
      */
     public function getTypeName(array $parameter, array $spec = []): string
     {
+        if (str_contains($parameter['description'], 'Collection attributes') || str_contains($parameter['description'], 'List of attributes')) {
+            return '[]any';
+        }
         return match ($parameter['type']) {
             self::TYPE_INTEGER => 'int',
             self::TYPE_NUMBER => 'float64',
