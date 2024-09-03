7fddaa74123619f5a7622991672b1d5ba66a009b
Jake Barnby <jakeb994@gmail.com>
2024-09-03 22:16:32 +1200

    Fix swift test

--- tests/languages/swift/Tests.swift
+++ tests/languages/swift/Tests.swift
@@ -70,7 +70,7 @@ class Tests: XCTestCase {
         print((result as! [String: Any])["result"] as! String)
 
         do {
-            var file = Payload.fromPath("\(FileManager.default.currentDirectoryPath)/../../resources/file.png")
+            var file = try Payload.fromPath("\(FileManager.default.currentDirectoryPath)/../../resources/file.png")
             mock = try await general.upload(x: "string", y: 123, z: ["string in array"], file: file, onProgress: nil)
             print(mock.result)
         } catch {
@@ -78,7 +78,7 @@ class Tests: XCTestCase {
         }
 
         do {
-            var file = Payload.fromPath("\(FileManager.default.currentDirectoryPath)/../../resources/large_file.mp4")
+            var file = try Payload.fromPath("\(FileManager.default.currentDirectoryPath)/../../resources/large_file.mp4")
             mock = try await general.upload(x: "string", y: 123, z: ["string in array"], file: file, onProgress: nil)
             print(mock.result)
         } catch {
