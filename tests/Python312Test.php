532ff5f4544fb79014a10147586200e0dd4c2d59
Christy Jacob <christyjacob4@gmail.com>
2025-03-10 17:33:42 +0530

    fix: deprecate support for python 3.8

--- tests/Python312Test.php
+++ tests/Python312Test.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-class Python38Test extends Base
+class Python312Test extends Base
 {
     protected string $sdkName = 'python';
     protected string $sdkPlatform = 'server';
@@ -14,10 +14,10 @@ class Python38Test extends Base
     protected array $build = [
         'cp tests/languages/python/tests.py tests/sdks/python/test.py',
         'echo "" > tests/sdks/python/__init__.py',
-        'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor python:3.8-alpine pip install -r tests/sdks/python/requirements.txt --upgrade',
+        'docker run --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor python:3.12-alpine pip install -r tests/sdks/python/requirements.txt --upgrade',
     ];
     protected string $command =
-        'docker run --network="mockapi" --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor --env PYTHONPATH=tests/sdks/python/vendor python:3.8-alpine python tests/sdks/python/test.py';
+        'docker run --network="mockapi" --rm -v $(pwd):/app -w /app --env PIP_TARGET=tests/sdks/python/vendor --env PYTHONPATH=tests/sdks/python/vendor python:3.12-alpine python tests/sdks/python/test.py';
 
     protected array $expectedOutput = [
         ...Base::FOO_RESPONSES,
