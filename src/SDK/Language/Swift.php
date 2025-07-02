b43861ef6c1a6c5a45b0c054043cf7d31b72e200
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-07-02 22:12:03 +0530

    chore: shift logic to filter

--- src/SDK/Language/Swift.php
+++ src/SDK/Language/Swift.php
@@ -478,6 +478,9 @@ class Swift extends Language
                 if (isset($this->getIdentifierOverrides()[$value])) {
                     $value = $this->getIdentifierOverrides()[$value];
                 }
+                if (preg_match('/^\\d/', $value)) {
+                    $value = '_' . $value;
+                }
                 return $this->toCamelCase($value);
             }),
         ];
