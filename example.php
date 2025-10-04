ace76917e4322fd8d2fa69d3273239b6935ddb4c
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-10-04 11:57:11 +0530

    cleanup more

--- example.php
+++ example.php
@@ -39,6 +39,7 @@ try {
     function configureSDK($sdk, $overrides = []) {
         $defaults = [
             'name' => 'NAME',
+            'version' => '0.0.0',
             'description' => 'Repo description goes here',
             'shortDescription' => 'Repo short description goes here',
             'url' => 'https://example.com',
@@ -51,11 +52,13 @@ try {
             'twitter' => 'appwrite_io',
             'discord' => ['564160730845151244', 'https://appwrite.io/discord'],
             'defaultHeaders' => ['X-Appwrite-Response-Format' => '1.6.0'],
+            'readme' => '**README**',
         ];
 
         $config = array_merge($defaults, $overrides);
 
         $sdk->setName($config['name'])
+            ->setVersion($config['version'])
             ->setDescription($config['description'])
             ->setShortDescription($config['shortDescription'])
             ->setURL($config['url'])
@@ -67,16 +70,11 @@ try {
             ->setGitRepoName($config['gitRepoName'])
             ->setTwitter($config['twitter'])
             ->setDiscord($config['discord'][0], $config['discord'][1])
-            ->setDefaultHeaders($config['defaultHeaders']);
+            ->setDefaultHeaders($config['defaultHeaders'])
+            ->setReadme($config['readme']);
 
-        if (isset($config['version'])) {
-            $sdk->setVersion($config['version']);
-        }
         if (isset($config['examples'])) {
             $sdk->setExamples($config['examples']);
-        }
-        if (isset($config['readme'])) {
-            $sdk->setReadme($config['readme']);
         }
         if (isset($config['license'])) {
             $sdk->setLicense($config['license']);
@@ -91,17 +89,14 @@ try {
         return $sdk;
     }
 
-    // Parse command-line arguments
     $requestedSdk = isset($argv[1]) ? $argv[1] : null;
     $requestedPlatform = isset($argv[2]) ? $argv[2] : null;
 
-    // Determine platform
     if ($requestedPlatform) {
         $platform = $requestedPlatform;
     } else {
-        // Leave the platform you want uncommented
-        // $platform = 'client';
         $platform = 'console';
+        // $platform = 'client';
         // $platform = 'server';
     }
 
@@ -123,32 +118,21 @@ try {
             ->setComposerVendor('appwrite')
             ->setComposerPackage('appwrite');
         $sdk  = new SDK($php, new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'logo' => 'https://appwrite.io/images/github.png',
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/php');
     }
 
     // Web
     if (!$requestedSdk || $requestedSdk === 'web') {
         $sdk  = new SDK(new Web(), new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'version' => '0.0.0',
-            'readme' => "## Getting Started",
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/web');
     }
 
     // Node
     if (!$requestedSdk || $requestedSdk === 'node') {
         $sdk  = new SDK(new Node(), new Swagger2($spec));
-
         configureSDK($sdk);
-
         $sdk->generate(__DIR__ . '/examples/node');
     }
 
@@ -175,14 +159,7 @@ try {
         |_|   |_|                                                ");
 
         $sdk  = new SDK($language, new Swagger2($spec));
-
         configureSDK($sdk, [
-            'version' => '0.16.0',
-            'url' => 'https://appwrite.io',
-            'license' => 'BSD-3-Clause',
-            'gitUserName' => 'appwrite',
-            'gitRepoName' => 'sdk-for-cli',
-            'defaultHeaders' => ['X-Appwrite-Response-Format' => '1.7.0'],
             'exclude' => [
                 'services' => [
                     ['name' => 'assistant'],
@@ -197,20 +174,14 @@ try {
     // Ruby
     if (!$requestedSdk || $requestedSdk === 'ruby') {
         $sdk  = new SDK(new Ruby(), new Swagger2($spec));
-
         configureSDK($sdk);
-
         $sdk->generate(__DIR__ . '/examples/ruby');
     }
 
     // Python
     if (!$requestedSdk || $requestedSdk === 'python') {
         $sdk  = new SDK(new Python(), new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'version' => '7.2.0',
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/python');
     }
 
@@ -218,13 +189,8 @@ try {
     if (!$requestedSdk || $requestedSdk === 'dart') {
         $dart = new Dart();
         $dart->setPackageName('dart_appwrite');
-
         $sdk  = new SDK($dart, new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'examples' => '**EXAMPLES** <HTML>',
-            'version' => '0.0.1',
-        ]);
+        configureSDK($sdk);
 
         $sdk->generate(__DIR__ . '/examples/dart');
     }
@@ -234,12 +200,7 @@ try {
         $flutter = new Flutter();
         $flutter->setPackageName('appwrite');
         $sdk  = new SDK($flutter, new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'examples' => '**EXAMPLES** <HTML>',
-            'version' => '0.0.1',
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/flutter');
     }
 
@@ -248,119 +209,72 @@ try {
         $reactNative = new ReactNative();
         $reactNative->setNPMPackage('react-native-appwrite');
         $sdk  = new SDK($reactNative, new Swagger2($spec));
-
         configureSDK($sdk, [
             'examples' => '**EXAMPLES** <HTML>',
             'version' => '0.0.1',
         ]);
-
         $sdk->generate(__DIR__ . '/examples/react-native');
     }
 
     // GO
     if (!$requestedSdk || $requestedSdk === 'go') {
         $sdk  = new SDK(new Go(), new Swagger2($spec));
-
         configureSDK($sdk, [
-            'version' => '0.0.1',
             'gitUserName' => 'appwrite',
             'gitRepoName' => 'sdk-for-go',
         ]);
-
         $sdk->generate(__DIR__ . '/examples/go');
     }
 
     // Swift
     if (!$requestedSdk || $requestedSdk === 'swift') {
         $sdk  = new SDK(new Swift(), new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'version' => '0.0.1',
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/swift');
     }
 
     // Apple
     if (!$requestedSdk || $requestedSdk === 'apple') {
         $sdk  = new SDK(new Apple(), new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'version' => '0.0.1',
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/apple');
     }
 
     // DotNet
     if (!$requestedSdk || $requestedSdk === 'dotnet') {
         $sdk  = new SDK(new DotNet(), new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'version' => '0.0.1',
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/dotnet');
     }
 
     // REST
     if (!$requestedSdk || $requestedSdk === 'rest') {
         $sdk  = new SDK(new REST(), new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'version' => '0.0.1',
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/REST');
     }
 
     // Android
     if (!$requestedSdk || $requestedSdk === 'android') {
         $sdk = new SDK(new Android(), new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'name' => 'Android',
-            'namespace' => 'io appwrite',
-            'description' => 'Appwrite is an open-source backend as a service server that abstract and simplify complex and repetitive development tasks behind a very simple to use REST API. Appwrite aims to help you develop your apps faster and in a more secure way. Use the Flutter SDK to integrate your app with the Appwrite server to easily start interacting with all of Appwrite backend APIs and tools. For full API documentation and tutorials go to https://appwrite.io/docs',
-            'shortDescription' => 'Appwrite Android SDK',
-            'gitUserName' => 'appwrite',
-            'gitRepoName' => 'sdk-for-android',
-            'warning' => '**This SDK is compatible with Appwrite server version 0.7.x. For older versions, please check previous releases.**',
-            'version' => '0.0.0-SNAPSHOT',
-            'defaultHeaders' => ['x-appwrite-response-format' => '0.7.0'],
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/android');
     }
 
     // Kotlin
     if (!$requestedSdk || $requestedSdk === 'kotlin') {
         $sdk = new SDK(new Kotlin(), new Swagger2($spec));
-
-        configureSDK($sdk, [
-            'name' => 'Kotlin',
-            'namespace' => 'io appwrite',
-            'description' => 'Appwrite is an open-source backend as a service server that abstract and simplify complex and repetitive development tasks behind a very simple to use REST API. Appwrite aims to help you develop your apps faster and in a more secure way. Use the Flutter SDK to integrate your app with the Appwrite server to easily start interacting with all of Appwrite backend APIs and tools. For full API documentation and tutorials go to https://appwrite.io/docs',
-            'shortDescription' => 'Appwrite Kotlin SDK',
-            'gitUserName' => 'appwrite',
-            'gitRepoName' => 'sdk-for-kotlin',
-            'warning' => '**This SDK is compatible with Appwrite server version 0.7.x. For older versions, please check previous releases.**',
-            'version' => '0.0.0-SNAPSHOT',
-            'defaultHeaders' => ['x-appwrite-response-format' => '0.8.0'],
-        ]);
-
+        configureSDK($sdk);
         $sdk->generate(__DIR__ . '/examples/kotlin');
     }
 
     // GraphQL
     if (!$requestedSdk || $requestedSdk === 'graphql') {
         $sdk = new SDK(new GraphQL(), new Swagger2($spec));
-
         $sdk
             ->setName('GraphQL')
             ->setDescription('Appwrite is an open-source backend as a service server that abstract and simplify complex and repetitive development tasks behind a very simple to use REST API. Appwrite aims to help you develop your apps faster and in a more secure way. Use the Flutter SDK to integrate your app with the Appwrite server to easily start interacting with all of Appwrite backend APIs and tools. For full API documentation and tutorials go to https://appwrite.io/docs')
             ->setLogo('https://appwrite.io/v1/images/console.png');
-
         $sdk->generate(__DIR__ . '/examples/graphql');
     }
 }
