c8ffdb5e1698d34875055121f550435a45213af4
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-10-26 16:24:37 +0530

    fix: lint

--- src/SDK/Language.php
+++ src/SDK/Language.php
@@ -138,15 +138,17 @@ abstract class Language
         return \strtoupper($this->toSnakeCase($str));
     }
 
-    function isPermissionString(string $string): bool {
+    function isPermissionString(string $string): bool
+    {
         $pattern = '/^\["(read|update|delete|write)\(\\"[^\\"]+\\"\)"\]$/';
         return preg_match($pattern, $string) === 1;
     }
 
-    function extractPermissionParts(string $string): array {
+    function extractPermissionParts(string $string): array
+    {
         $inner = substr($string, 1, -1);
         preg_match_all('/"(read|update|delete|write)\(\\"([^\\"]+)\\"\)"/', $inner, $matches, PREG_SET_ORDER);
-        
+
         $result = [];
         foreach ($matches as $match) {
             $result[] = [
@@ -154,7 +156,7 @@ abstract class Language
                 'role' => $match[2]
             ];
         }
-        
+
         return $result;
     }
 
