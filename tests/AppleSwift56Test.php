b0533b1a0124fbaddb4bbd4f92e3e8b08cf76e93
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-01-09 16:39:05 +0530

    fixed failing swift tests

--- tests/AppleSwift56Test.php
+++ tests/AppleSwift56Test.php
@@ -16,7 +16,7 @@ class AppleSwift56Test extends Base
         'cp tests/languages/apple/Tests.swift tests/sdks/apple/Tests/AppwriteTests/Tests.swift',
     ];
     protected string $command =
-        'docker run --network="mockapi" --rm -v $(pwd):/app -w /app/tests/sdks/apple swift:5.6-focal swift test 2>&1';
+        'docker run --network="mockapi" --rm -v $(pwd):/app -w /app/tests/sdks/apple swift:5.6-focal swift test';
 
     protected array $expectedOutput = [
         ...Base::PING_RESPONSE,
