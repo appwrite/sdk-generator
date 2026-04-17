9a974aa2af16d3dcfd52d92c85a5c725738a63a8
bhardwajparth51 <196071556+bhardwajparth51@users.noreply.github.com>
2026-04-17 11:37:44 +0530

    test(cpp): add gtest-based unit tests for models and helpers

---
+++ tests/Cpp20Test.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests;
+
+class Cpp20Test extends Base
+{
+    protected string $sdkName = "cpp";
+    protected string $sdkPlatform = "server";
+    protected string $sdkLanguage = "cpp";
+    protected string $version = "0.0.1";
+
+    protected string $language = "cpp";
+    protected string $class = "Appwrite\SDK\Language\Cpp";
+    protected array $build = [
+        "mkdir -p tests/tmp/cpp",
+        "cp -Rf tests/sdks/cpp/* tests/tmp/cpp/",
+        "docker build -t appwrite-cpp-env tests/languages/cpp/"
+    ];
+    protected string $command = 'docker run --network="mockapi" --rm -v $(pwd):/app -w /app appwrite-cpp-env sh -c "cd tests/languages/cpp/ && chmod +x ./test.sh && ./test.sh"';
+    protected array $expectedOutput = [
+        ...Base::FOO_RESPONSES,
+        ...Base::BAR_RESPONSES,
+        ...Base::GENERAL_RESPONSES,
+        ...Base::UPLOAD_RESPONSES,
+        ...Base::DOWNLOAD_RESPONSES,
+        ...Base::EXCEPTION_RESPONSES,
+        ...Base::QUERY_HELPER_RESPONSES,
+        ...Base::PERMISSION_HELPER_RESPONSES,
+        ...Base::ID_HELPER_RESPONSES,
+    ];
+}
---
+++ tests/languages/cpp/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+mkdir -p /tmp/cpp-sdk
+cp -Rf /app/tests/tmp/cpp/* /tmp/cpp-sdk/
+
+# Copy test resource files so integration_test can find them by relative path
+mkdir -p /tmp/cpp-sdk/tests/resources
+cp /app/tests/resources/file.png /tmp/cpp-sdk/tests/resources/file.png
+cp /app/tests/resources/large_file.mp4 /tmp/cpp-sdk/tests/resources/large_file.mp4
+
+cd /tmp/cpp-sdk || exit 1
+
+# Build SDK and integration test binary
+cmake -S . -B build -DAPPWRITE_BUILD_TESTS=ON
+cmake --build build
+
+# Run unit tests (model serialization, helper classes)
+./build/test_appwrite
+
+# Run integration test from the project root so relative paths resolve correctly
+./build/integration_test
