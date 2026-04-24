d56eddd438ee1e9555af508736a9205ca0841b25
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-04-24 11:26:36 +0530

    added react native tests

---
+++ tests/ReactNativeTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests;
+
+class ReactNativeTest extends Base
+{
+    protected string $sdkName = 'react-native';
+    protected string $sdkPlatform = 'client';
+    protected string $sdkLanguage = 'reactnative';
+    protected string $version = '0.0.1';
+
+    protected string $language = 'react-native';
+    protected string $class = 'Appwrite\SDK\Language\ReactNative';
+
+    protected array $build = [
+        'cp tests/languages/react-native/tests.js tests/sdks/react-native/tests.js',
+        'cp tests/languages/react-native/index.html tests/sdks/react-native/index.html',
+        'cp tests/languages/react-native/browser.js tests/sdks/react-native/browser.js',
+        'cp tests/languages/react-native/rollup.test.config.mjs tests/sdks/react-native/rollup.test.config.mjs',
+        'mkdir -p tests/sdks/react-native/shims && cp tests/languages/react-native/shims/expo-file-system.js tests/sdks/react-native/shims/expo-file-system.js',
+        'docker run --rm -v $(pwd):/app -w /app/tests/sdks/react-native mcr.microsoft.com/playwright:v1.56.1-jammy sh -c "npm install && npm install --no-save react-native-web react react-dom @rollup/plugin-alias @rollup/plugin-commonjs @rollup/plugin-node-resolve @rollup/plugin-replace serve-handler"',
+        'docker run --rm -v $(pwd):/app -w /app/tests/sdks/react-native mcr.microsoft.com/playwright:v1.56.1-jammy sh -c "npx rollup -c rollup.test.config.mjs"',
+    ];
+
+    protected string $command =
+        'docker run --network="mockapi" --rm -v $(pwd):/app -e BROWSER=chromium -w /app/tests/sdks/react-native mcr.microsoft.com/playwright:v1.56.1-jammy node tests.js';
+
+    protected array $expectedOutput = [
+        ...Base::PING_RESPONSE,
+        ...Base::FOO_RESPONSES,
+        ...Base::FOO_RESPONSES, // Object params
+        ...Base::BAR_RESPONSES,
+        ...Base::BAR_RESPONSES, // Object params
+        ...Base::GENERAL_RESPONSES,
+        ...Base::ENUM_RESPONSES,
+        ...Base::MODEL_RESPONSES,
+        ...Base::EXCEPTION_RESPONSES,
+        ...Base::REALTIME_RESPONSES,
+        ...Base::QUERY_HELPER_RESPONSES,
+        ...Base::PERMISSION_HELPER_RESPONSES,
+        ...Base::ID_HELPER_RESPONSES,
+        ...Base::CHANNEL_HELPER_RESPONSES,
+        ...Base::OPERATOR_HELPER_RESPONSES,
+    ];
+}
---
+++ tests/languages/react-native/browser.js
@@ -0,0 +1,345 @@
+import {
+    Client,
+    Foo,
+    Bar,
+    General,
+    Realtime,
+    Query,
+    Permission,
+    Role,
+    ID,
+    Channel,
+    Operator,
+    Condition,
+    MockType,
+} from './src/index';
+
+(async () => {
+    let response;
+    let responseRealtime = 'Realtime failed!';
+    let responseRealtimeWithQueries = 'Realtime failed!';
+    let responseRealtimeWithQueriesFailure = 'Realtime failed!';
+
+    let rtsubFailureUnsubscribed = false;
+    let rtsubFailureFiredAfterUnsubscribe = false;
+
+    const client = new Client();
+
+    const foo = new Foo(client);
+    const bar = new Bar(client);
+    const general = new General(client);
+    const sdkHeaders = client.getHeaders();
+
+    console.log(
+        `x-sdk-name: ${sdkHeaders['x-sdk-name']}; x-sdk-platform: ${sdkHeaders['x-sdk-platform']}; x-sdk-language: ${sdkHeaders['x-sdk-language']}; x-sdk-version: ${sdkHeaders['x-sdk-version']}`
+    );
+
+    client.setProject('123456');
+    response = await client.ping();
+    console.log(response.result);
+
+    client.setProject('console');
+    client.setEndpointRealtime('wss://cloud.appwrite.io/v1');
+
+    const realtime = new Realtime(client);
+
+    await realtime.subscribe(['tests'], (message) => {
+        responseRealtime = message.payload.response;
+    });
+
+    const rtsubWithQueries = await realtime.subscribe(
+        ['tests'],
+        (message) => {
+            responseRealtimeWithQueries = message.payload.response;
+        },
+        [[Query.equal('response', ['WS:/v1/realtime:passed'])]],
+    );
+
+    const rtsubWithQueriesFailure = await realtime.subscribe(
+        ['tests'],
+        (message) => {
+            if (rtsubFailureUnsubscribed) rtsubFailureFiredAfterUnsubscribe = true;
+            responseRealtimeWithQueriesFailure = message.payload.response;
+        },
+        [[Query.equal('response', ['failed'])]],
+    );
+
+    // Foo
+    response = await foo.get('string', 123, ['string in array']);
+    console.log(response.result);
+    response = await foo.post('string', 123, ['string in array']);
+    console.log(response.result);
+    response = await foo.put('string', 123, ['string in array']);
+    console.log(response.result);
+    response = await foo.patch('string', 123, ['string in array']);
+    console.log(response.result);
+    response = await foo.delete('string', 123, ['string in array']);
+    console.log(response.result);
+
+    // Foo (Object params)
+    response = await foo.get({ x: 'string', y: 123, z: ['string in array'] });
+    console.log(response.result);
+    response = await foo.post({ x: 'string', y: 123, z: ['string in array'] });
+    console.log(response.result);
+    response = await foo.put({ x: 'string', y: 123, z: ['string in array'] });
+    console.log(response.result);
+    response = await foo.patch({ x: 'string', y: 123, z: ['string in array'] });
+    console.log(response.result);
+    response = await foo.delete({ x: 'string', y: 123, z: ['string in array'] });
+    console.log(response.result);
+
+    // Bar
+    response = await bar.get('string', 123, ['string in array']);
+    console.log(response.result);
+    response = await bar.post('string', 123, ['string in array']);
+    console.log(response.result);
+    response = await bar.put('string', 123, ['string in array']);
+    console.log(response.result);
+    response = await bar.patch('string', 123, ['string in array']);
+    console.log(response.result);
+    response = await bar.delete('string', 123, ['string in array']);
+    console.log(response.result);
+
+    // Bar (Object params)
+    response = await bar.get({ required: 'string', xdefault: 123, z: ['string in array'] });
+    console.log(response.result);
+    response = await bar.post({ required: 'string', xdefault: 123, z: ['string in array'] });
+    console.log(response.result);
+    response = await bar.put({ required: 'string', xdefault: 123, z: ['string in array'] });
+    console.log(response.result);
+    response = await bar.patch({ required: 'string', xdefault: 123, z: ['string in array'] });
+    console.log(response.result);
+    response = await bar.delete({ required: 'string', xdefault: 123, z: ['string in array'] });
+    console.log(response.result);
+
+    // General
+    response = await general.redirect();
+    console.log(response.result);
+
+    // Enum
+    response = await general.enum(MockType.First);
+    console.log(response.result);
+
+    // Request model tests
+    response = await general.createPlayer({ id: 'player1', name: 'John Doe', score: 100 });
+    console.log(response.result);
+    response = await general.createPlayers([
+        { id: 'player1', name: 'John Doe', score: 100 },
+        { id: 'player2', name: 'Jane Doe', score: 200 },
+    ]);
+    console.log(response.result);
+
+    // Exception responses
+    try {
+        response = await general.error400();
+    } catch (error) {
+        console.log(error.message);
+        console.log(error.response);
+    }
+    try {
+        response = await general.error500();
+    } catch (error) {
+        console.log(error.message);
+        console.log(error.response);
+    }
+    try {
+        response = await general.error502();
+    } catch (error) {
+        console.log(error.message);
+        console.log(error.response);
+    }
+    try {
+        client.setEndpoint('htp://cloud.appwrite.io/v1');
+    } catch (error) {
+        console.log(error.message);
+    }
+
+    // Realtime waits
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+    await delay(5000);
+    console.log(responseRealtime);
+    console.log(responseRealtimeWithQueries);
+    console.log(responseRealtimeWithQueriesFailure);
+
+    try {
+        await rtsubWithQueriesFailure.unsubscribe();
+        rtsubFailureUnsubscribed = true;
+        await rtsubWithQueriesFailure.unsubscribe();
+        await delay(500);
+        if (rtsubFailureFiredAfterUnsubscribe) {
+            throw new Error('callback fired after unsubscribe');
+        }
+        console.log('Realtime unsubscribe:passed');
+    } catch (e) {
+        console.log('Realtime unsubscribe:failed');
+    }
+
+    try {
+        await rtsubWithQueries.update({ channels: ['tests'], queries: [] });
+        console.log('Realtime update:passed');
+    } catch (e) {
+        console.log('Realtime update:failed');
+    }
+
+    try {
+        await realtime.disconnect();
+        console.log('Realtime disconnect:passed');
+    } catch (e) {
+        console.log('Realtime disconnect:failed');
+    }
+
+    // Query helper tests
+    console.log(Query.equal('released', [true]));
+    console.log(Query.equal('title', ['Spiderman', 'Dr. Strange']));
+    console.log(Query.notEqual('title', 'Spiderman'));
+    console.log(Query.lessThan('releasedYear', 1990));
+    console.log(Query.greaterThan('releasedYear', 1990));
+    console.log(Query.search('name', 'john'));
+    console.log(Query.isNull('name'));
+    console.log(Query.isNotNull('name'));
+    console.log(Query.between('age', 50, 100));
+    console.log(Query.between('age', 50.5, 100.5));
+    console.log(Query.between('name', 'Anna', 'Brad'));
+    console.log(Query.startsWith('name', 'Ann'));
+    console.log(Query.endsWith('name', 'nne'));
+    console.log(Query.select(['name', 'age']));
+    console.log(Query.orderAsc('title'));
+    console.log(Query.orderDesc('title'));
+    console.log(Query.orderRandom());
+    console.log(Query.cursorAfter('my_movie_id'));
+    console.log(Query.cursorBefore('my_movie_id'));
+    console.log(Query.limit(50));
+    console.log(Query.offset(20));
+    console.log(Query.contains('title', 'Spider'));
+    console.log(Query.contains('labels', 'first'));
+    console.log(Query.containsAny('labels', ['first', 'second']));
+    console.log(Query.containsAll('labels', ['first', 'second']));
+
+    console.log(Query.notContains('title', 'Spider'));
+    console.log(Query.notSearch('name', 'john'));
+    console.log(Query.notBetween('age', 50, 100));
+    console.log(Query.notStartsWith('name', 'Ann'));
+    console.log(Query.notEndsWith('name', 'nne'));
+    console.log(Query.createdBefore('2023-01-01'));
+    console.log(Query.createdAfter('2023-01-01'));
+    console.log(Query.createdBetween('2023-01-01', '2023-12-31'));
+    console.log(Query.updatedBefore('2023-01-01'));
+    console.log(Query.updatedAfter('2023-01-01'));
+    console.log(Query.updatedBetween('2023-01-01', '2023-12-31'));
+
+    console.log(Query.distanceEqual('location', [[40.7128, -74], [40.7128, -74]], 1000));
+    console.log(Query.distanceEqual('location', [40.7128, -74], 1000, true));
+    console.log(Query.distanceNotEqual('location', [40.7128, -74], 1000));
+    console.log(Query.distanceNotEqual('location', [40.7128, -74], 1000, true));
+    console.log(Query.distanceGreaterThan('location', [40.7128, -74], 1000));
+    console.log(Query.distanceGreaterThan('location', [40.7128, -74], 1000, true));
+    console.log(Query.distanceLessThan('location', [40.7128, -74], 1000));
+    console.log(Query.distanceLessThan('location', [40.7128, -74], 1000, true));
+
+    console.log(Query.intersects('location', [40.7128, -74]));
+    console.log(Query.notIntersects('location', [40.7128, -74]));
+    console.log(Query.crosses('location', [40.7128, -74]));
+    console.log(Query.notCrosses('location', [40.7128, -74]));
+    console.log(Query.overlaps('location', [40.7128, -74]));
+    console.log(Query.notOverlaps('location', [40.7128, -74]));
+    console.log(Query.touches('location', [40.7128, -74]));
+    console.log(Query.notTouches('location', [40.7128, -74]));
+    console.log(Query.contains('location', [[40.7128, -74], [40.7128, -74]]));
+    console.log(Query.notContains('location', [[40.7128, -74], [40.7128, -74]]));
+    console.log(Query.equal('location', [[40.7128, -74], [40.7128, -74]]));
+    console.log(Query.notEqual('location', [[40.7128, -74], [40.7128, -74]]));
+
+    console.log(Query.or([
+        Query.equal('released', true),
+        Query.lessThan('releasedYear', 1990),
+    ]));
+    console.log(Query.and([
+        Query.equal('released', false),
+        Query.greaterThan('releasedYear', 2015),
+    ]));
+
+    console.log(Query.regex('name', 'pattern.*'));
+    console.log(Query.exists(['attr1', 'attr2']));
+    console.log(Query.notExists(['attr1', 'attr2']));
+    console.log(Query.elemMatch('friends', [
+        Query.equal('name', 'Alice'),
+        Query.greaterThan('age', 18),
+    ]));
+
+    // Permission & Role helper tests
+    console.log(Permission.read(Role.any()));
+    console.log(Permission.write(Role.user(ID.custom('userid'))));
+    console.log(Permission.create(Role.users()));
+    console.log(Permission.update(Role.guests()));
+    console.log(Permission.delete(Role.team('teamId', 'owner')));
+    console.log(Permission.delete(Role.team('teamId')));
+    console.log(Permission.create(Role.member('memberId')));
+    console.log(Permission.update(Role.users('verified')));
+    console.log(Permission.update(Role.user(ID.custom('userid'), 'unverified')));
+    console.log(Permission.create(Role.label('admin')));
+
+    // ID helper tests
+    console.log(ID.unique());
+    console.log(ID.custom('custom_id'));
+
+    // Channel helper tests
+    console.log(Channel.database('db1').collection('col1').document().toString());
+    console.log(Channel.database('db1').collection('col1').document('doc1').toString());
+    console.log(Channel.database('db1').collection('col1').document('doc1').create().toString());
+    console.log(Channel.database('db1').collection('col1').document('doc1').upsert().toString());
+    console.log(Channel.tablesdb('db1').table('table1').row().toString());
+    console.log(Channel.tablesdb('db1').table('table1').row('row1').toString());
+    console.log(Channel.tablesdb('db1').table('table1').row('row1').update().toString());
+    console.log(Channel.account());
+    console.log(Channel.bucket('bucket1').file().toString());
+    console.log(Channel.bucket('bucket1').file('file1').toString());
+    console.log(Channel.bucket('bucket1').file('file1').delete().toString());
+    console.log(Channel.function('func2').toString());
+    console.log(Channel.function('func1').toString());
+    console.log(Channel.execution('exec2').toString());
+    console.log(Channel.execution('exec1').toString());
+    console.log(Channel.documents());
+    console.log(Channel.rows());
+    console.log(Channel.files());
+    console.log(Channel.executions());
+    console.log(Channel.teams());
+    console.log(Channel.team('team2').toString());
+    console.log(Channel.team('team1').toString());
+    console.log(Channel.team('team1').create().toString());
+    console.log(Channel.memberships());
+    console.log(Channel.membership('membership2').toString());
+    console.log(Channel.membership('membership1').toString());
+    console.log(Channel.membership('membership1').update().toString());
+
+    // Operator helper tests
+    console.log(Operator.increment(1));
+    console.log(Operator.increment(5, 100));
+    console.log(Operator.decrement(1));
+    console.log(Operator.decrement(3, 0));
+    console.log(Operator.multiply(2));
+    console.log(Operator.multiply(3, 1000));
+    console.log(Operator.divide(2));
+    console.log(Operator.divide(4, 1));
+    console.log(Operator.modulo(5));
+    console.log(Operator.power(2));
+    console.log(Operator.power(3, 100));
+    console.log(Operator.arrayAppend(['item1', 'item2']));
+    console.log(Operator.arrayPrepend(['first', 'second']));
+    console.log(Operator.arrayInsert(0, 'newItem'));
+    console.log(Operator.arrayRemove('oldItem'));
+    console.log(Operator.arrayUnique());
+    console.log(Operator.arrayIntersect(['a', 'b', 'c']));
+    console.log(Operator.arrayDiff(['x', 'y']));
+    console.log(Operator.arrayFilter(Condition.Equal, 'test'));
+    console.log(Operator.stringConcat('suffix'));
+    console.log(Operator.stringReplace('old', 'new'));
+    console.log(Operator.toggle());
+    console.log(Operator.dateAddDays(7));
+    console.log(Operator.dateSubDays(3));
+    console.log(Operator.dateSetNow());
+
+    window.__APPWRITE_TEST_DONE__ = true;
+})().catch((err) => {
+    console.log('TEST RUNNER ERROR: ' + (err && err.message ? err.message : String(err)));
+    window.__APPWRITE_TEST_DONE__ = true;
+});
