2211b0b26086162fa963429864097c957d52889b
Nikhil Verma <nikhil2003verma@gmail.com>
2026-05-08 00:55:40 +0530

    feat: godot basic project for ping test

--- src/SDK/Language/Godot.php
+++ src/SDK/Language/Godot.php
@@ -38,128 +38,153 @@ class Godot extends GDScript
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/plugin.cfg',
-                'template' => 'godot/src/plugin.cfg.twig',
+                'template' => 'godot/addons/plugin.cfg.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/plugin.gd',
-                'template' => 'godot/src/plugin.gd.twig',
+                'template' => 'godot/addons/plugin.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/{{ spec.title | caseSnake }}.gd',
-                'template' => 'godot/src/appwrite.gd.twig',
+                'template' => 'godot/addons/appwrite.gd.twig',
             ],
             [
-                'scope' => 'default',
+                'scope' => 'copy',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/icon.svg',
-                'template' => 'godot/src/icon.svg',
+                'template' => 'godot/addons/icon.svg',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/client.gd',
-                'template' => 'godot/src/client.gd.twig',
+                'template' => 'godot/addons/client.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/service.gd',
-                'template' => 'godot/src/service.gd.twig',
+                'template' => 'godot/addons/service.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/exception.gd',
-                'template' => 'godot/src/exception.gd.twig',
+                'template' => 'godot/addons/exception.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/id.gd',
-                'template' => 'godot/src/id.gd.twig',
+                'template' => 'godot/addons/id.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/permission.gd',
-                'template' => 'godot/src/permission.gd.twig',
+                'template' => 'godot/addons/permission.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/role.gd',
-                'template' => 'godot/src/role.gd.twig',
+                'template' => 'godot/addons/role.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/query.gd',
-                'template' => 'godot/src/query.gd.twig',
+                'template' => 'godot/addons/query.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/input_file.gd',
-                'template' => 'godot/src/input_file.gd.twig',
-            ],
-            [
-                'scope' => 'default',
-                'destination' => 'addons/{{ spec.title | caseSnake }}/.env.example',
-                'template' => 'godot/example.env.twig'
+                'template' => 'godot/addons/input_file.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'addons/{{spec.title | caseSnake}}/operator.gd',
-                'template' => 'godot/src/operator.gd.twig',
+                'template' => 'godot/addons/operator.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'tests/test_query.gd',
-                'template' => 'godot/src/tests/test_query.gd.twig',
+                'template' => 'godot/addons/tests/test_query.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'tests/test_roles.gd',
-                'template' => 'godot/src/tests/test_roles.gd.twig',
+                'template' => 'godot/addons/tests/test_roles.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'tests/test_query.gd',
-                'template' => 'godot/src/tests/test_query.gd.twig',
+                'template' => 'godot/addons/tests/test_query.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'tests/test_id.gd',
-                'template' => 'godot/src/tests/test_id.gd.twig',
+                'template' => 'godot/addons/tests/test_id.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'tests/test_permission.gd',
-                'template' => 'godot/src/tests/test_permission.gd.twig',
+                'template' => 'godot/addons/tests/test_permission.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'tests/test_input_files.gd',
-                'template' => 'godot/src/tests/test_input_files.gd.twig',
+                'template' => 'godot/addons/tests/test_input_files.gd.twig',
             ],
             [
                 'scope' => 'default',
                 'destination' => 'tests/test_operator.gd',
-                'template' => 'godot/src/tests/test_operator.gd.twig',
+                'template' => 'godot/addons/tests/test_operator.gd.twig',
+            ],
+            [
+                'scope' => 'default',
+                'destination' => '.env',
+                'template' => 'godot/.env.twig',
             ],
             [
                 'scope' => 'enum',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/enums/{{ enum.name | caseSnake }}.gd',
-                'template' => 'godot/src/enums/enum.gd.twig',
+                'template' => 'godot/addons/enums/enum.gd.twig',
             ],
             [
                 'scope' => 'service',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/services/{{ service.name | caseSnake }}.gd',
-                'template' => 'godot/src/services/service.gd.twig',
+                'template' => 'godot/addons/services/service.gd.twig',
             ],
             [
                 'scope' => 'definition',
                 'destination' => 'addons/{{ spec.title | caseSnake }}/models/{{ definition.name | caseSnake }}.gd',
-                'template' => 'godot/src/models/model.gd.twig',
+                'template' => 'godot/addons/models/model.gd.twig',
             ],
             [
                 'scope' => 'method',
                 'destination' => 'docs/examples/{{service.name | caseSnake}}/{{method.name | caseSnake}}.md',
                 'template' => 'godot/docs/example.md.twig',
             ],
+            [
+                'scope' => 'copy',
+                'destination' => '.editorconfig',
+                'template' => 'godot/.editorconfig',
+            ],
+            [
+                'scope' => 'copy',
+                'destination' => 'menu.gd',
+                'template' => 'godot/menu.gd',
+            ],
+            [
+                'scope' => 'copy',
+                'destination' => 'Menu.tscn',
+                'template' => 'godot/Menu.tscn',
+            ],
+            [
+                'scope' => 'default',
+                'destination' => 'project.godot',
+                'template' => 'godot/project.godot.twig',
+            ],
+            [
+                'scope' => 'copy',
+                'destination' => 'icon.svg',
+                'template' => 'godot/addons/icon.svg',
+            ]
         ];
     }
 }
