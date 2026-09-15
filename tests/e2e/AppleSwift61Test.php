fdd3ecf9438fb548b6e92a881e894f1644695bf7
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-09-15 17:45:53 +0530

    test(apple): e2e push round-trip against the MQTT broker

--- tests/e2e/AppleSwift61Test.php
+++ tests/e2e/AppleSwift61Test.php
@@ -51,6 +51,7 @@ final class AppleSwift61Test extends Base
         ...Base::PERMISSION_HELPER_RESPONSES,
         ...Base::ID_HELPER_RESPONSES,
         ...Base::CHANNEL_HELPER_RESPONSES,
-        ...Base::OPERATOR_HELPER_RESPONSES
+        ...Base::OPERATOR_HELPER_RESPONSES,
+        ...Base::PUSH_RESPONSES
     ];
 }
