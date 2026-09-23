8f3b57e3116034cb7e3d2edec9d6b2e234b33dc1
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-09-23 15:21:33 +0530

    feat(dotnet): add the Topic helper

--- tests/e2e/DotNet90Test.php
+++ tests/e2e/DotNet90Test.php
@@ -47,6 +47,7 @@ final class DotNet90Test extends Base
         ...Base::QUERY_HELPER_RESPONSES,
         ...Base::PERMISSION_HELPER_RESPONSES,
         ...Base::ID_HELPER_RESPONSES,
+        ...Base::TOPIC_HELPER_RESPONSES,
         ...Base::OPERATOR_HELPER_RESPONSES
     ];
 }
