85924c7bbfcae38a5d60d25f4acfb80c4c1bc88f
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-09-24 13:52:50 +0100

    fix(go): preserve nullable scalar response fields

--- tests/e2e/languages/go-v2/test.sh
+++ tests/e2e/languages/go-v2/test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+set -e
 mkdir -p /go/src/github.com/repoowner/reponame/v2/
 cp -Rf /app/tests/e2e/sdks/go/* /go/src/github.com/repoowner/reponame/v2/
 
+go test github.com/repoowner/reponame/v2/models
 go run tests.go
--- tests/e2e/languages/go/test.sh
+++ tests/e2e/languages/go/test.sh
@@ -4,5 +4,5 @@ set -e
 mkdir -p /go/src/github.com/repoowner/reponame/
 cp -Rf /app/tests/e2e/sdks/go/* /go/src/github.com/repoowner/reponame/
 
-go test github.com/repoowner/reponame/client
+go test github.com/repoowner/reponame/client github.com/repoowner/reponame/models
 go run tests.go
