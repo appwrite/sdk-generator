12f7abe9aba24f18b22b755d0fcc4bdb166e054d
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-09-24 14:06:09 +0100

    test(go): decouple nullable response regression from generator

--- tests/e2e/languages/go-v2/test.sh
+++ tests/e2e/languages/go-v2/test.sh
@@ -3,5 +3,6 @@ set -e
 mkdir -p /go/src/github.com/repoowner/reponame/v2/
 cp -Rf /app/tests/e2e/sdks/go/* /go/src/github.com/repoowner/reponame/v2/
 
+cp /app/tests/e2e/languages/go/models/nullable_test.go /go/src/github.com/repoowner/reponame/v2/models/
 go test github.com/repoowner/reponame/v2/models
 go run tests.go
--- tests/e2e/languages/go/test.sh
+++ tests/e2e/languages/go/test.sh
@@ -4,5 +4,6 @@ set -e
 mkdir -p /go/src/github.com/repoowner/reponame/
 cp -Rf /app/tests/e2e/sdks/go/* /go/src/github.com/repoowner/reponame/
 
+cp /app/tests/e2e/languages/go/models/nullable_test.go /go/src/github.com/repoowner/reponame/models/
 go test github.com/repoowner/reponame/client github.com/repoowner/reponame/models
 go run tests.go
